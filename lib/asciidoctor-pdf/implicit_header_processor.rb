f890f361919f8f2ca224b8baa0a2d30fdfa293f8
Dan Allen <dan.j.allen@gmail.com>
2014-05-28 20:40:41 -0600

    add and register implicit header processor;

---
+++ lib/asciidoctor-pdf/implicit_header_processor.rb
@@ -0,0 +1,63 @@
+require 'asciidoctor'
+require 'asciidoctor/extensions'
+
+module Asciidoctor
+module Pdf
+# An include processor that skips the implicit author line below
+# the document title in documents which are included.
+class ImplicitHeaderProcessor < ::Asciidoctor::Extensions::IncludeProcessor
+  def initialize document
+    @document = document
+  end
+
+  def process reader, target, attributes
+    return reader unless File.exist? target
+    ::File.open target, 'r' do |fd|
+      # FIXME handle case where doc id is specified above title
+      if (first_line = fd.readline) && (first_line.start_with? '= ')
+        # HACK reset counters for each article for Asciidoctor Editions
+        if (doc = @document).attr? 'env-editions'
+          doc.counters.each do |(counter_key, counter_val)|
+            doc.attributes.delete counter_key
+          end
+          doc.counters.clear
+        end
+        if (second_line = fd.readline)
+          if AuthorInfoLineRx =~ second_line
+            # FIXME temporary hack to set author and e-mail attributes; this should handle all attributes in header!
+            author = [$1, $2, $3].compact * ' '
+            email = $4
+            reader.push_include fd.readlines, target, target, 3, attributes unless fd.eof?
+            reader.push_include first_line, target, target, 1, attributes
+            lines = [%(:author: #{author})]
+            lines << %(:email: #{email}) if email
+            reader.push_include lines, target, target, 2, attributes
+          else
+            lines = [second_line]
+            lines += fd.readlines unless fd.eof?
+            reader.push_include lines, target, target, 2, attributes
+            reader.push_include first_line, target, target, 1, attributes
+          end
+        else
+          reader.push_include first_line, target, target, 1, attributes
+        end
+      else
+        lines = [first_line]
+        lines += fd.readlines unless fd.eof?
+        reader.push_include lines, target, target, 1, attributes
+      end
+    end
+    reader
+  end
+
+  def handles? target
+    target != 'bio.adoc' && ((target.end_with? '.adoc') || (target.end_with? '.asciidoc'))
+  end
+
+  # FIXME this method shouldn't be required
+  def update_config config
+    (@config ||= {}).update config
+  end
+end
+end
+end
