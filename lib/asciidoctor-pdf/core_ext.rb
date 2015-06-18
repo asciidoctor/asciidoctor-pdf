df896c7b8e3d856cd0b7b38e56fbf2fb94ef405a
Dan Allen <dan.j.allen@gmail.com>
2015-06-18 01:22:20 -0600

    patch OpenStruct if RUBY_VERSION < '2.0.0'

--- lib/asciidoctor-pdf/core_ext.rb
+++ lib/asciidoctor-pdf/core_ext.rb
@@ -1,5 +1,2 @@
 require_relative 'core_ext/array'
 require_relative 'core_ext/numeric'
-if RUBY_ENGINE == 'rbx'
-  require_relative 'core_ext/ostruct'
-end
--- lib/asciidoctor-pdf/core_ext/ostruct.rb
+++ lib/asciidoctor-pdf/core_ext/ostruct.rb
@@ -6,4 +6,4 @@ class OpenStruct
   def []= key, val
     send %(#{key}=), val
   end unless respond_to? :[]=
-end
+end if RUBY_ENGINE == 'rbx' || RUBY_VERSION < '2.0.0'
