8a6a9b665367a06a9cfcd408c37908cb8a58e34d
Dan Allen <dan.j.allen@gmail.com>
2016-08-04 23:31:51 -0600

    resolves #471 fix compatibility with Rouge >= 1.11.1 (PR #493)

--- lib/asciidoctor-pdf/rouge_ext/css_theme.rb
+++ lib/asciidoctor-pdf/rouge_ext/css_theme.rb
@@ -1,7 +1,8 @@
 module Rouge
 class CSSTheme
   # Patch style_for to return most specific style first
-  # See https://github.com/jneen/rouge/issues/280 (fix pending)
+  # See https://github.com/jneen/rouge/issues/280
+  # Fixed as of rouge v1.10.0
   def style_for token
     token.token_chain.reverse_each do |t|
       if (s = styles[t])
@@ -11,4 +12,4 @@ class CSSTheme
     nil
   end
 end
-end
+end if (Gem::Version.new Rouge.version) < (Gem::Version.new '1.10.0')
