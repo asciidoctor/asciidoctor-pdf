c8603639703dfc8578a049ffb5e9a90150e394a4
Dan Allen <dan.j.allen@gmail.com>
2017-01-30 23:36:01 -0700

    drop background colors on strings in rouge pastie theme
    - the background colors don't look professional, so drop them

--- lib/asciidoctor-pdf/rouge_ext/themes/pastie.rb
+++ lib/asciidoctor-pdf/rouge_ext/themes/pastie.rb
@@ -5,9 +5,11 @@ module Rouge
     class Pastie < CSSTheme
       name 'pastie' 
 
-      style Comment,                   fg: '#888888'
+      # Deviate from pastie here since our italic is actually a thinner font
+      style Comment,                   fg: '#888888' #, italic: true
       style Comment::Preproc,          fg: '#cc0000', bold: true
-      style Comment::Special,          fg: '#cc0000', bg: '#fff0f0', bold: true
+      # Deviate from pastie here by not using a background color
+      style Comment::Special,          fg: '#cc0000', bold: true #, bg: '#fff0f0'
 
       style Error,                     fg: '#a61717', bg: '#e3d2d2'
       style Generic::Error,            fg: '#aa0000'
@@ -32,14 +34,13 @@ module Rouge
 
       style Num,                       fg: '#0000dd', bold: true
 
-      style Str,                       fg: '#dd2200', bg: '#fff0f0'
-      style Str::Escape,               fg: '#0044dd', bg: '#fff0f0'
-      style Str::Interpol,             fg: '#3333bb', bg: '#fff0f0'
-      style Str::Other,                fg: '#22bb22', bg: '#f0fff0'
-      #style Str::Regex,               fg: '#008800', bg: '#fff0ff'
-      # The background color on regex really doesn't look good, so let's drop it
-      style Str::Regex,                fg: '#008800'
-      style Str::Symbol,               fg: '#aa6600', bg: '#fff0f0'
+      # Deviate from pastie here by not using background colors
+      style Str,                       fg: '#dd2200' #, bg: '#fff0f0'
+      style Str::Escape,               fg: '#0044dd' #, bg: '#fff0f0'
+      style Str::Interpol,             fg: '#3333bb' #, bg: '#fff0f0'
+      style Str::Other,                fg: '#22bb22' #, bg: '#f0fff0'
+      style Str::Regex,                fg: '#008800' #, bg: '#fff0ff'
+      style Str::Symbol,               fg: '#aa6600' #, bg: '#fff0f0'
 
       style Name::Attribute,           fg: '#336699'
       style Name::Builtin,             fg: '#003388'
@@ -48,9 +49,8 @@ module Rouge
       style Name::Decorator,           fg: '#555555'
       style Name::Exception,           fg: '#bb0066', bold: true
       style Name::Function,            fg: '#0066bb', bold: true
-      #style Name::Label,              fg: '#336699', italic: true
       # Name::Label is used for built-in CSS properties in Rouge, so let's drop italics
-      style Name::Label,               fg: '#336699'
+      style Name::Label,               fg: '#336699' #, italic: true
       style Name::Namespace,           fg: '#bb0066', bold: true
       style Name::Property,            fg: '#336699', bold: true
       style Name::Tag,                 fg: '#bb0066', bold: true
