aabab918a653757cfba08f17617dd37c19ab1bbf
Dan Allen <dan.j.allen@gmail.com>
2017-02-15 23:56:30 -0700

    remove unused variable

--- lib/asciidoctor-pdf/converter.rb
+++ lib/asciidoctor-pdf/converter.rb
@@ -1614,7 +1614,7 @@ class Converter < ::Prawn::Document
     }
 
     # QUESTION should we support nth; should we support sequence of roles?
-    case (stripe = node.attr 'stripe', 'even', false)
+    case node.attr 'stripe', 'even', false
     when 'all'
       table_settings[:row_colors] = [body_stripe_bg_color]
     when 'even'
