8d0877af8c2b028d8cb7780f03b6d25568e66db0
Dan Allen <dan.j.allen@gmail.com>
2018-05-02 19:18:46 -0600

    fix copyright

--- lib/asciidoctor-pdf/roman_numeral.rb
+++ lib/asciidoctor-pdf/roman_numeral.rb
@@ -23,7 +23,7 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 # Copyright (c) 2011 Andrew Vos
-# Copyright (c) 2014 OpenDevice, Inc.
+# Copyright (c) 2014 OpenDevise, Inc.
 #
 ########################################################################
 
