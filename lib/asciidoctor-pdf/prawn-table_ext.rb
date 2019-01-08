748f4ea4aee96933426e6cfda944944c32e40ef1
Dan Allen <dan.j.allen@gmail.com>
2019-01-07 19:54:27 -0700

    resolves #990 allow table border color to be transparent

--- lib/asciidoctor-pdf/prawn-table_ext.rb
+++ lib/asciidoctor-pdf/prawn-table_ext.rb
@@ -1,3 +1,4 @@
 require 'prawn/table' unless defined? Prawn::Table::VERSION
+require_relative 'prawn-table_ext/cell'
 require_relative 'prawn-table_ext/cell/asciidoc'
 require_relative 'prawn-table_ext/cell/text'
