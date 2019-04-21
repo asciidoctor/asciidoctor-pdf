117b7a38abc343d0c8c3f9828a7b6fdbb8b9192a
Dan Allen <dan.j.allen@gmail.com>
2019-04-21 02:45:30 -0600

    resolves #447 fix cannot fit error caused by zwsp at start of table cell
    - Prawn >= 2.2.0 resolves cannot fit error for table cell that starts with zwsp in non-default font
    - add test to verify

---
+++ spec/tables_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'spec_helper'
+
+describe 'Asciidoctor::Pdf::Converter - Tables' do
+  it 'should not fail to fit text in cell' do
+    pdf = to_pdf <<~'EOS', attributes: 'nofooter', analyze: :text
+    |===
+    |Aaaaa Bbbbb Ccccc |*{zwsp}* Aaaaa_Bbbbb_Ccccc |Aaaaa_Bbbbb_Ccccc |Aaaaa_Bbbbb_Ccccc |A
+    |===
+    EOS
+    (expect pdf.strings.index 'Aaaaa Bbbbb').to eql 0
+    (expect pdf.strings.index 'Ccccc').to eql 1
+  end
+end
