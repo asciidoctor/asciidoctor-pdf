b8e87875948c230b06f5f60e2cc0aacc3c252da7
Dan Allen <dan.j.allen@gmail.com>
2019-04-23 02:22:27 -0600

    use squiggly heredoc for pdfmark [skip ci]

--- lib/asciidoctor-pdf/pdfmark.rb
+++ lib/asciidoctor-pdf/pdfmark.rb
@@ -10,16 +10,16 @@ class Pdfmark
   def generate
     doc = @doc
     # FIXME use sanitize: :plain_text once available
-    content = <<-EOS
-[ /Title #{sanitize(doc.doctitle use_fallback: true).to_pdf}
-  /Author #{(doc.attr 'authors').to_pdf}
-  /Subject #{(doc.attr 'subject').to_pdf}
-  /Keywords #{(doc.attr 'keywords').to_pdf}
-  /ModDate #{date = ::Time.now.to_pdf}
-  /CreationDate #{date}
-  /Creator (Asciidoctor PDF #{::Asciidoctor::Pdf::VERSION}, based on Prawn #{::Prawn::VERSION})
-  /Producer #{(doc.attr 'publisher').to_pdf}
-  /DOCINFO pdfmark
+    content = <<~EOS
+    [ /Title #{sanitize(doc.doctitle use_fallback: true).to_pdf}
+      /Author #{(doc.attr 'authors').to_pdf}
+      /Subject #{(doc.attr 'subject').to_pdf}
+      /Keywords #{(doc.attr 'keywords').to_pdf}
+      /ModDate #{date = ::Time.now.to_pdf}
+      /CreationDate #{date}
+      /Creator (Asciidoctor PDF #{::Asciidoctor::Pdf::VERSION}, based on Prawn #{::Prawn::VERSION})
+      /Producer #{(doc.attr 'publisher').to_pdf}
+      /DOCINFO pdfmark
     EOS
     content
   end
