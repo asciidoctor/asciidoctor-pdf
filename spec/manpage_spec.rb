8da35c07c1137c9e06195edee96f41a403069fc9
Dan Allen <dan.j.allen@gmail.com>
2019-04-27 03:18:37 -0600

    fix broken test when running against Asciidoctor 1.5.3

--- spec/manpage_spec.rb
+++ spec/manpage_spec.rb
@@ -29,7 +29,7 @@ describe 'Asciidoctor::Pdf::Converter - Manpage' do
     name_body_text = (pdf.find_text 'cmd - does stuff')[0]
     (expect name_body_text).not_to be_nil
     (expect name_body_text[:font_size]).to eql 10.5
-    (expect (pdf.find_text font_size: 22).map {|it| it[:string] }).to eql %w(Name Synopsis Options)
+    (expect (pdf.find_text font_size: 22).map {|it| it[:string] }).to eql [expected_name_title, 'Synopsis', 'Options']
   end
 
   it 'should uppercase title of name section if other sections are uppercase' do
