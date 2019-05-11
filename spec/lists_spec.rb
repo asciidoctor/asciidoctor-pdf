57f323e87fee375817b6ee6026ba4f7ed9878b38
Dan Allen <dan.j.allen@gmail.com>
2019-05-10 22:23:02 -0600

    add first test for ordered list

--- spec/lists_spec.rb
+++ spec/lists_spec.rb
@@ -1,7 +1,20 @@
 require_relative 'spec_helper'
 
 describe 'Asciidoctor::PDF::Converter - Lists' do
-  context 'dlist' do
+  context 'Ordered List' do
+    it 'should number list items using arabic numbering by default' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      . a
+      . b
+      . c
+      EOS
+
+      (expect pdf.strings).to eql %w(1. a 2. b 3. c)
+      (expect pdf.lines).to eql ['1.a', '2.b', '3.c']
+    end
+  end
+
+  context 'Description List' do
     it 'should convert qanda to ordered list' do
       pdf = to_pdf <<~'EOS', analyze: true
       [qanda]
