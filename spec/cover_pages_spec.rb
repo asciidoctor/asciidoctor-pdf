507cc31a2b68a2759658bb725dcb0dfb8780be3f
Dan Allen <dan.j.allen@gmail.com>
2019-05-27 23:48:47 -0600

    add test to verify front cover is scaled until it covers page

--- spec/cover_pages_spec.rb
+++ spec/cover_pages_spec.rb
@@ -14,8 +14,8 @@ describe 'Asciidoctor::PDF::Converter - Cover Pages' do
     (expect pdf.pages[0][:text]).to be_empty
   end
 
-  it 'should scale front cover image to fit width of page', integration: true do
-    to_file = to_pdf_file <<~'EOS', 'cover-pages-front-cover.pdf', attributes: { 'imagesdir' => fixtures_dir }
+  it 'should scale front cover image to fit page', integration: true do
+    to_file = to_pdf_file <<~'EOS', 'cover-pages-front-cover-exact.pdf', attributes: { 'imagesdir' => fixtures_dir }
     = Document Title
     :doctype: book
     :front-cover-image: image:cover.jpg[]
@@ -23,6 +23,19 @@ describe 'Asciidoctor::PDF::Converter - Cover Pages' do
     content page
     EOS
 
-    (expect to_file).to visually_match 'cover-pages-front-cover.pdf'
+    (expect to_file).to visually_match 'cover-pages-front-cover-exact.pdf'
+  end
+
+  it 'should scale and clip front cover image to cover whole page', integration: true do
+    to_file = to_pdf_file <<~'EOS', 'cover-pages-front-cover-clipped.pdf', attributes: { 'imagesdir' => fixtures_dir }
+    = Document Title
+    :doctype: book
+    :front-cover-image: image:cover.jpg[]
+    :pdf-page-size: Legal
+
+    content page
+    EOS
+
+    (expect to_file).to visually_match 'cover-pages-front-cover-clipped.pdf'
   end
 end
