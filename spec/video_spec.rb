39487b850a2a7a906443c46820787d837fefe522
Dan Allen <dan.j.allen@gmail.com>
2019-05-30 23:33:53 -0600

    validate that poster is used for YouTube video if available and enabled

--- spec/video_spec.rb
+++ spec/video_spec.rb
@@ -10,4 +10,14 @@ describe 'Asciidoctor::PDF::Converter - Image' do
       (expect to_file).to visually_match 'video-local-file-poster.pdf'
     end
   end
+
+  context 'YouTube' do
+    it 'should replace image with poster image if allow-uri-read attribute is set', integration: true do
+      to_file = to_pdf_file <<~'EOS', 'video-youtube-poster.pdf', attributes: { 'imagesdir' => fixtures_dir, 'allow-uri-read' => '', 'nofooter' => '' }
+      video::49tpIMDy9BE[youtube,pdfwidth=100%]
+      EOS
+
+      (expect to_file).to visually_match 'video-youtube-poster.pdf'
+    end
+  end
 end
