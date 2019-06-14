90cd9cdf378340b6e404373ce80679b1b712fcad
Dan Allen <dan.j.allen@gmail.com>
2019-06-13 23:58:36 -0600

    add test for Rouge lexer options

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,6 +1,39 @@
 require_relative 'spec_helper'
 
 describe 'Asciidoctor::PDF::Converter - Source' do
+  context 'Rouge' do
+    it 'should enable start_inline option for PHP by default' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      :source-highlighter: rouge
+
+      [source,php]
+      ----
+      echo "<?php";
+      ----
+      EOS
+
+      echo_text = (pdf.find_text 'echo')[0]
+      (expect echo_text).not_to be_nil
+      # NOTE the echo keyword should be highlighted
+      (expect echo_text[:font_color]).to eql '008800'
+    end
+
+    it 'should not enable the start_inline option for PHP if the mixed option is set' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      :source-highlighter: rouge
+
+      [source%mixed,php]
+      ----
+      echo "<?php";
+      ----
+      EOS
+
+      echo_text = (pdf.find_text 'echo')[0]
+      # NOTE the echo keyword should not be highlighted
+      (expect echo_text).to be_nil
+    end
+  end
+
   context 'Callouts' do
     it 'should substitute autonumber callouts with circled numbers when using rouge as syntax highlighter' do
       pdf = to_pdf <<~'EOS', analyze: true
