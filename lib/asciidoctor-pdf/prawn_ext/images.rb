e9fee1a03928f7c9d92b41ea29ba74891c10b28f
Dan Allen <dan.j.allen@gmail.com>
2019-06-29 18:28:13 -0600

    resolves #765 scale SVG background image to fit page (PR #1116)

--- lib/asciidoctor-pdf/prawn_ext/images.rb
+++ lib/asciidoctor-pdf/prawn_ext/images.rb
@@ -9,10 +9,18 @@ module Images
 
   # Dispatch to suitable image method in Prawn based on file extension.
   def image file, opts = {}
-    # FIXME handle case when SVG is a File or IO object
+    # FIXME handle case when SVG is an IO object
     if ::String === file && (file.downcase.end_with? '.svg')
       opts[:fallback_font_name] ||= default_svg_font if respond_to? :default_svg_font
-      svg((::File.read file), opts)
+      if (opts.key? :fit) && (fit = opts.delete :fit) && !opts[:width] && !opts[:height]
+        svg (::File.read file), opts do |svg_doc|
+          max_width, max_height = fit
+          svg_doc.calculate_sizing requested_width: max_width if max_width && svg_doc.sizing.output_width != max_width
+          svg_doc.calculate_sizing requested_height: max_height if max_height && svg_doc.sizing.output_height > max_height
+        end
+      else
+        svg (::File.read file), opts
+      end
     else
       _initial_image file, opts
     end
