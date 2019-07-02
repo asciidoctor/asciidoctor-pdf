cfd93068cd33ac754c5b42dcb31f357d1e5b14bf
Dan Allen <dan.j.allen@gmail.com>
2019-07-02 00:43:36 -0600

    add test for block macro syntax for front cover image

--- spec/cover_page_spec.rb
+++ spec/cover_page_spec.rb
@@ -26,6 +26,18 @@ describe 'Asciidoctor::PDF::Converter - Cover Page' do
     (expect to_file).to visually_match 'cover-page-front-cover-exact.pdf'
   end
 
+  it 'should recognize attribute value that uses block macro syntax', integration: true do
+    to_file = to_pdf_file <<~'EOS', 'cover-page-front-cover-block-macro.pdf', attributes: { 'imagesdir' => fixtures_dir }
+    = Document Title
+    :doctype: book
+    :front-cover-image: image::cover.jpg[]
+
+    content page
+    EOS
+
+    (expect to_file).to visually_match 'cover-page-front-cover-exact.pdf'
+  end
+
   it 'should scale and clip front cover image to cover whole page', integration: true do
     to_file = to_pdf_file <<~'EOS', 'cover-page-front-cover-clipped.pdf', attributes: { 'imagesdir' => fixtures_dir }
     = Document Title
