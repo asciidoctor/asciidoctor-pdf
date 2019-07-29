24a64eba13a9fa612630d6d8def19bead8534cf2
Dan Allen <dan.j.allen@gmail.com>
2019-07-28 23:57:24 -0600

    add test for autofit option on listing (and literal) blocks

--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -13,4 +13,28 @@ describe 'Asciidoctor::PDF::Converter - Listing' do
     listing_page_numbers = (pdf.find_text 'listing').map {|it| it[:page_number] }.uniq
     (expect listing_page_numbers).to eql [2]
   end
+
+  it 'should resize font size to prevent wrapping if autofit option is set' do
+    pdf = to_pdf <<~'EOS', analyze: true
+    [%autofit]
+    ----
+    theme = ThemeLoader.load_theme theme_name, (theme_dir = (doc.attr 'pdf-themesdir') || (doc.attr 'pdf-stylesdir'))
+    ----
+    EOS
+
+    (expect pdf.text).to have_size 1
+    (expect pdf.text[0][:font_size]).to be < build_pdf_theme.code_font_size
+  end
+
+  it 'should not resize font size more than minimum font size' do
+    pdf = to_pdf <<~'EOS', pdf_theme: { base_font_size_min: 8 }, analyze: true
+    [%autofit]
+    ----
+    play_symbol = (node.document.attr? 'icons', 'font') ? %(<font name="fas">#{(icon_font_data 'fas').unicode 'play'}</font>) : RightPointer
+    ----
+    EOS
+
+    (expect pdf.text).to have_size 2
+    (expect pdf.text[0][:font_size]).to eql 8
+  end
 end
