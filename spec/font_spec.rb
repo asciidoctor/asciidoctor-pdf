4499ad8aef520ebb640c99cd86f2114153b5f50b
Dan Allen <dan.j.allen@gmail.com>
2019-08-12 23:56:09 -0600

    resolves #1206 if value of scripts attribute is cjk, break lines between any two CJK characters (PR #1207)

--- spec/font_spec.rb
+++ spec/font_spec.rb
@@ -141,6 +141,39 @@ describe 'Asciidoctor::PDF::Converter - Font' do
     end
   end
 
+  context 'Line breaks' do
+    it 'should break line on any CJK character if value of scripts attribute is cjk' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      :scripts: cjk
+      :pdf-theme: default-with-fallback-font
+
+      AsciiDoc 是一个人类可读的文件格式，语义上等同于 DocBook 的 XML，但使用纯文本标记了约定。可以使用任何文本编辑器创建文件把 AsciiDoc 和阅读“原样”，或呈现为HTML 或由 DocBook 的工具链支持的任何其他格式，如 PDF，TeX 的，Unix 的手册页，电子书，幻灯片演示等。
+
+      AsciiDoc は、意味的には DocBook XML のに相当するが、プレーン·テキスト·マークアップの規則を使用して、人間が読めるドキュメントフォーマット、である。 AsciiDoc は文書は、任意のテキストエディタを使用して作成され、「そのまま"または、HTML や DocBook のツールチェーンでサポートされている他のフォーマット、すなわち PDF、TeX の、Unix の man ページ、電子書籍、スライドプレゼンテーションなどにレンダリングすることができます。
+      EOS
+
+      lines = pdf.lines
+      (expect lines).to have_size 8
+      (expect lines[0]).to end_with '任何'
+      (expect lines[1]).to start_with '文本'
+      (expect lines[3]).to end_with '使用'
+      (expect lines[4]).to start_with 'して'
+    end
+
+    it 'should not break line immediately before an ideographic full stop' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      :scripts: cjk
+      :pdf-theme: default-with-fallback-font
+
+      Asciidoctor PDF 是一个 Asciidoctor 转换器，可将 AsciiDoc 文档转换为PDF文档。填料填料。转换器不会创建临时格式。
+      EOS
+
+      lines = pdf.lines
+      (expect lines).to have_size 2
+      (expect lines[1]).to start_with '式。'
+    end
+  end
+
   context 'Separators' do
     it 'should not break line at location of no-break space' do
       input = (%w(a b c d).reduce([]) {|accum, it| accum << (it * 20) }.join ' ') + ?\u00a0 + ('e' * 20)
