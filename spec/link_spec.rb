b0711db84fbfb4eca461bd8c8e566edaf0371534
Dan Allen <dan.j.allen@gmail.com>
2019-08-18 23:59:35 -0600

    verify character references in link href are decoded

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -17,6 +17,15 @@ describe 'Asciidoctor::PDF::Converter - Link' do
     (expect link_text[:x]).to eql link_annotation[:Rect][0]
   end
 
+  it 'should decode character references in the href' do
+    input = 'https://github.com/asciidoctor/asciidoctor-pdf/milestones?direction=asc&sort=<>&state=open'
+    pdf = to_pdf input
+    text = (pdf.page 1).text
+    (expect text).to eql input
+    link = (get_annotations pdf, 1)[0]
+    (expect link[:A][:URI]).to eql input
+  end
+
   it 'should reveal URL of link when media=print or media=prepress' do
     %w(print prepress).each do |media|
       pdf = to_pdf <<~'EOS', attribute_overrides: { 'media' => 'print' }, analyze: true
