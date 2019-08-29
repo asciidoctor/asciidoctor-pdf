68229c2dedd90ef4f0ad1e0211a080f7f405bc00
Dan Allen <dan.j.allen@gmail.com>
2019-08-28 23:18:31 -0600

    add test to verify listing block indentation is preserved and tabs are replaced

--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -83,4 +83,67 @@ describe 'Asciidoctor::PDF::Converter - Listing' do
     (expect lines[0]).to eql expected_line
     (expect lines[2]).to eql expected_line
   end
+
+  it 'should guard indentation using no-break space character' do
+    pdf = to_pdf <<~EOS, analyze: true
+    ----
+    flush
+      indented
+    flush
+    ----
+    EOS
+
+    (expect pdf.lines).to eql ['flush', %(\u00a0 indented), 'flush']
+  end
+
+  it 'should replace tabs with spaces if tabsize is specified as block attribute' do
+    pdf = to_pdf <<~EOS, analyze: true
+    [tabsize=4]
+    ----
+    flush
+      lead space
+    \tlead tab
+    \tlead tab\tcolumn tab
+      lead space\tcolumn tab
+    flush\t\t\tcolumn tab
+    ----
+    EOS
+
+    expected_lines = [
+      'flush',
+      %(\u00a0 lead space),
+      %(\u00a0   lead tab),
+      %(\u00a0   lead tab    column tab),
+      %(\u00a0 lead space    column tab),
+      'flush           column tab',
+    ]
+
+    (expect pdf.lines).to eql expected_lines
+  end
+
+  it 'should replace tabs with spaces if tabsize is specified as document attribute' do
+    pdf = to_pdf <<~EOS, analyze: true
+    :tabsize: 4
+
+    ----
+    flush
+      lead space
+    \tlead tab
+    \tlead tab\tcolumn tab
+      lead space\tcolumn tab
+    flush\t\t\tcolumn tab
+    ----
+    EOS
+
+    expected_lines = [
+      'flush',
+      %(\u00a0 lead space),
+      %(\u00a0   lead tab),
+      %(\u00a0   lead tab    column tab),
+      %(\u00a0 lead space    column tab),
+      'flush           column tab',
+    ]
+
+    (expect pdf.lines).to eql expected_lines
+  end
 end
