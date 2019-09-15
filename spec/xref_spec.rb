a8af37ed24883ae8d0ff75e8af138099b9a83b44
Dan Allen <dan.j.allen@gmail.com>
2019-09-14 23:55:20 -0600

    verify internal xrefs work

--- spec/xref_spec.rb
+++ spec/xref_spec.rb
@@ -1,7 +1,111 @@
 require_relative 'spec_helper'
 
 describe 'Asciidoctor::PDF::Converter - Xref' do
-  context 'Interdocument' do
+  context 'internal' do
+    it 'should create reference to a section by title' do
+      pdf = to_pdf <<~'EOS'
+      = Document Title
+      :doctype: book
+
+      == Chapter A
+
+      You can find details in <<Chapter B>>.
+
+      == Chapter B
+
+      Here are the details you're looking for.
+      EOS
+
+      names = get_names pdf
+      (expect names).to have_key '_chapter_a'
+      (expect names).to have_key '_chapter_b'
+      annotations = get_annotations pdf, 2
+      (expect annotations).to have_size 1
+      (expect annotations[0][:Dest]).to eql '_chapter_b'
+      (expect (pdf.page 2).text).to include 'Chapter B'
+    end
+
+    it 'should create reference to a section by implicit ID' do
+      pdf = to_pdf <<~'EOS'
+      = Document Title
+      :doctype: book
+
+      == Chapter A
+
+      You can find details in <<_chapter_b>>.
+
+      == Chapter B
+
+      Here are the details you're looking for.
+      EOS
+
+      names = get_names pdf
+      (expect names).to have_key '_chapter_a'
+      (expect names).to have_key '_chapter_b'
+      annotations = get_annotations pdf, 2
+      (expect annotations).to have_size 1
+      (expect annotations[0][:Dest]).to eql '_chapter_b'
+      (expect (pdf.page 2).text).to include 'Chapter B'
+    end
+
+    it 'should create reference to a section by explicit ID' do
+      pdf = to_pdf <<~'EOS'
+      = Document Title
+      :doctype: book
+
+      [#a]
+      == Chapter A
+
+      You can find details in <<b>>.
+
+      [#b]
+      == Chapter B
+
+      Here are the details you're looking for.
+      EOS
+
+      names = get_names pdf
+      (expect names).to have_key 'a'
+      (expect names).to have_key 'b'
+      annotations = get_annotations pdf, 2
+      (expect annotations).to have_size 1
+      (expect annotations[0][:Dest]).to eql 'b'
+      (expect (pdf.page 2).text).to include 'Chapter B'
+    end
+
+    it 'should create reference to a block by explicit ID' do
+      pdf = to_pdf <<~'EOS'
+      = Document Title
+      :doctype: book
+
+      == Summary
+
+      You can find the observed values in the <<observed-values,table>>.
+
+      == Data
+
+      [#observed-values]
+      |===
+      | Subject | Count
+
+      | foo
+      | 2
+
+      | bar
+      | 1
+      |===
+      EOS
+
+      names = get_names pdf
+      (expect names).to have_key 'observed-values'
+      annotations = get_annotations pdf, 2
+      (expect annotations).to have_size 1
+      (expect annotations[0][:Dest]).to eql 'observed-values'
+      (expect (pdf.page 2).text).to include 'table'
+    end
+  end
+
+  context 'interdocument' do
     it 'should convert interdocument xrefs to internal references' do
       input_file = Pathname.new fixture_file 'book.adoc'
       pdf = to_pdf input_file
@@ -14,6 +118,14 @@ describe 'Asciidoctor::PDF::Converter - Xref' do
       first_steps_ref = p3_annotations[0]
       (expect first_steps_ref[:Subtype]).to eql :Link
       (expect first_steps_ref[:Dest]).to eql '_first_steps'
+    end
+
+    it 'should link self-referencing interdocument xref to built-in __anchor-top ref' do
+      pdf = to_pdf Pathname.new fixture_file 'reference-to-self.adoc'
+      (expect Pathname.new output_file 'reference-to-self.pdf').to exist
+      annotations = get_annotations pdf
+      (expect annotations).to have_size 1
+      (expect annotations[0][:Dest]).to eql '__anchor-top'
     end
   end
 
