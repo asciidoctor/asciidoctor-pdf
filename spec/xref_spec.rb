088fc0af4cd1e6488cc7b00b7d5b95ebff92c4cc
Dan Allen <dan.j.allen@gmail.com>
2019-10-08 23:50:36 -0600

    verify xrefstyle is honored when making reference to image

--- spec/xref_spec.rb
+++ spec/xref_spec.rb
@@ -247,6 +247,32 @@ describe 'Asciidoctor::PDF::Converter - Xref' do
       (expect pdf.lines).to include 'Start with Chapter 1, A.'
     end if asciidoctor_1_5_7_or_better?
 
+    it 'should refer to image with title by title by default' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      See <<img>>.
+
+      .Title of Image
+      [#img]
+      image::tux.png[]
+      EOS
+
+      (expect pdf.lines[0]).to eql 'See Title of Image.'
+    end if asciidoctor_1_5_7_or_better?
+
+    it 'should refer to image with title by reference signifier, number, and title when xrefstyle is full' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      :xrefstyle: full
+
+      See <<img>>.
+
+      .Title of Image
+      [#img]
+      image::tux.png[]
+      EOS
+
+      (expect pdf.lines[0]).to eql 'See Figure 1, “Title of Image”.'
+    end if asciidoctor_1_5_7_or_better?
+
     it 'should refer to image with title by reference signifier and number when xrefstyle is short' do
       pdf = to_pdf <<~'EOS', analyze: true
       :xrefstyle: short
