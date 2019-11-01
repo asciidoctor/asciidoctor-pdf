2fbe7bdb7abdcae821c13ab085706144393076d6
Dan Allen <dan.j.allen@gmail.com>
2019-10-31 23:30:17 -0600

    autoload FileUtils in test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ end
 require 'asciidoctor/pdf'
 require 'base64'
 require 'chunky_png'
+require 'fileutils' unless defined? FileUtils
 require 'open3' unless defined? Open3
 require 'pathname' unless defined? Pathname
 require 'pdf/inspector'
