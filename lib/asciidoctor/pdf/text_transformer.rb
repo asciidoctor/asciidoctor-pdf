3254e70be1561045da35f232a76b59ecc000440c
Dan Allen <dan.j.allen@gmail.com>
2019-11-16 00:50:30 -0700

    sort functions in TextTransformer class [skip ci]

--- lib/asciidoctor/pdf/text_transformer.rb
+++ lib/asciidoctor/pdf/text_transformer.rb
@@ -18,22 +18,6 @@ module TextTransformer
   WordRx = /\S+/
   SoftHyphen = ?\u00ad
 
-  def uppercase_pcdata string
-    if XMLMarkupRx.match? string
-      string.gsub(PCDATAFilterRx) { $2 ? (uppercase_mb $2) : $1 }
-    else
-      uppercase_mb string
-    end
-  end
-
-  def lowercase_pcdata string
-    if string.include? '<'
-      string.gsub(TagFilterRx) { $2 ? (lowercase_mb $2) : $1 }
-    else
-      lowercase_mb string
-    end
-  end
-
   def capitalize_words_pcdata string
     if XMLMarkupRx.match? string
       string.gsub(PCDATAFilterRx) { $2 ? (capitalize_words_mb $2) : $1 }
@@ -46,59 +30,6 @@ module TextTransformer
     string.gsub(WordRx) { capitalize_mb $& }
   end
 
-  if RUBY_VERSION >= '2.4'
-    def uppercase_mb string
-      string.upcase
-    end
-
-    def lowercase_mb string
-      string.downcase
-    end
-
-    def capitalize_mb string
-      string.capitalize
-    end
-  # NOTE Unicode library is 4x as fast as ActiveSupport::MultiByte::Chars
-  elsif defined? ::Unicode
-    def uppercase_mb string
-      string.ascii_only? ? string.upcase : (::Unicode.upcase string)
-    end
-
-    def lowercase_mb string
-      string.ascii_only? ? string.downcase : (::Unicode.downcase string)
-    end
-
-    def capitalize_mb string
-      string.ascii_only? ? string.capitalize : (::Unicode.capitalize string)
-    end
-  elsif defined? ::ActiveSupport::Multibyte
-    MultibyteChars = ::ActiveSupport::Multibyte::Chars
-
-    def uppercase_mb string
-      string.ascii_only? ? string.upcase : (MultibyteChars.new string).upcase.to_s
-    end
-
-    def lowercase_mb string
-      string.ascii_only? ? string.downcase : (MultibyteChars.new string).downcase.to_s
-    end
-
-    def capitalize_mb string
-      string.ascii_only? ? string.capitalize : (MultibyteChars.new string).capitalize.to_s
-    end
-  else
-    def uppercase_mb string
-      string.upcase
-    end
-
-    def lowercase_mb string
-      string.downcase
-    end
-
-    def capitalize_mb string
-      string.capitalize
-    end
-  end
-
   def hyphenate_words_pcdata string, hyphenator
     if XMLMarkupRx.match? string
       string.gsub(PCDATAFilterRx) { $2 ? (hyphenate_words $2, hyphenator) : $1 }
@@ -109,6 +40,75 @@ module TextTransformer
 
   def hyphenate_words string, hyphenator
     string.gsub(WordRx) { hyphenator.visualize $&, SoftHyphen }
+  end
+
+  def lowercase_pcdata string
+    if string.include? '<'
+      string.gsub(TagFilterRx) { $2 ? (lowercase_mb $2) : $1 }
+    else
+      lowercase_mb string
+    end
+  end
+
+  def uppercase_pcdata string
+    if XMLMarkupRx.match? string
+      string.gsub(PCDATAFilterRx) { $2 ? (uppercase_mb $2) : $1 }
+    else
+      uppercase_mb string
+    end
+  end
+
+  if RUBY_VERSION >= '2.4'
+    def capitalize_mb string
+      string.capitalize
+    end
+
+    def lowercase_mb string
+      string.downcase
+    end
+
+    def uppercase_mb string
+      string.upcase
+    end
+  # NOTE Unicode library is 4x as fast as ActiveSupport::MultiByte::Chars
+  elsif defined? ::Unicode
+    def capitalize_mb string
+      string.ascii_only? ? string.capitalize : (::Unicode.capitalize string)
+    end
+
+    def lowercase_mb string
+      string.ascii_only? ? string.downcase : (::Unicode.downcase string)
+    end
+
+    def uppercase_mb string
+      string.ascii_only? ? string.upcase : (::Unicode.upcase string)
+    end
+  elsif defined? ::ActiveSupport::Multibyte
+    MultibyteChars = ::ActiveSupport::Multibyte::Chars
+
+    def capitalize_mb string
+      string.ascii_only? ? string.capitalize : (MultibyteChars.new string).capitalize.to_s
+    end
+
+    def lowercase_mb string
+      string.ascii_only? ? string.downcase : (MultibyteChars.new string).downcase.to_s
+    end
+
+    def uppercase_mb string
+      string.ascii_only? ? string.upcase : (MultibyteChars.new string).upcase.to_s
+    end
+  else
+    def capitalize_mb string
+      string.capitalize
+    end
+
+    def lowercase_mb string
+      string.downcase
+    end
+
+    def uppercase_mb string
+      string.upcase
+    end
   end
 end
 end
