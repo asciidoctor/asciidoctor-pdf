accd046915511d9b544947a5f3f319fe7921e378
Dan Allen <dan.j.allen@gmail.com>
2019-11-27 23:58:08 -0700

    add tests for merging adjacent text nodes in formatted text transformer

--- spec/formatted_text_transform_spec.rb
+++ spec/formatted_text_transform_spec.rb
@@ -11,4 +11,22 @@ describe Asciidoctor::PDF::FormattedText::Transform do
     (expect fragments[0][:text]).to eql 'write tests!'
     (expect fragments[0][:styles]).to eql [:bold].to_set
   end
+
+  it 'should not merge adjacent text nodes by default' do
+    input = 'foo<br>bar'
+    parsed = parser.parse input
+    fragments = subject.apply parsed.content
+    (expect fragments).to have_size 3
+    (expect fragments[0][:text]).to eql 'foo'
+    (expect fragments[1][:text]).to eql ?\n
+    (expect fragments[2][:text]).to eql 'bar'
+  end
+
+  it 'should merge adjacent text nodes if specified' do
+    input = 'foo<br>bar'
+    parsed = parser.parse input
+    fragments = (subject.class.new merge_adjacent_text_nodes: true).apply parsed.content
+    (expect fragments).to have_size 1
+    (expect fragments[0][:text]).to eql %(foo\nbar)
+  end
 end
