b51348e4027fbcdbd323e1a952e9731e1d22a28a
Dan Allen <dan.j.allen@gmail.com>
2019-11-28 21:10:11 -0700

    move open block specs to dedicated spec file

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,57 +1,6 @@
 require_relative 'spec_helper'
 
 describe 'Asciidoctor::PDF::Converter - Example' do
-  it 'should keep block together when it has the unbreakable option' do
-    to_file = to_pdf_file <<~EOS, 'example-unbreakable-option-fit.pdf'
-    Make it rain.footnote:[money]
-
-    #{(['filler'] * 21).join %(\n\n)}
-    [%unbreakable]
-    --
-    To install Antora, open a terminal and type:
-    
-     $ npm i -g @antora/cli@2.2 @antora/site-generator-default@2.2
-    
-    IMPORTANT: The `@` at the beginning of the package name is important.
-    It tells `npm` that the `cli` package is located in the `antora` group.
-    If you omit this character, `npm` will assume the package name is the name of a git repository on GitHub.
-    The second `@` offsets the requested version number.footnote:[Clarification about this statement.]
-    Only the major and minor segments are specified to ensure you receive the latest patch update.
-    --
-
-    Make it snow.footnote:[dollar bills]
-    EOS
-
-    (expect to_file).to visually_match 'example-unbreakable-option-fit.pdf'
-  end
-
-  it 'should break an unbreakable block if it does not fit on one page' do
-    to_file = to_pdf_file <<~EOS, 'example-unbreakable-option-break.pdf'
-    Make it rain.footnote:[money]
-
-    #{(['filler'] * 21).join %(\n\n)}
-
-    [%unbreakable]
-    --
-    To install Antora, open a terminal and type:
-    
-     $ npm i -g @antora/cli@2.2 @antora/site-generator-default@2.2
-    
-    IMPORTANT: The `@` at the beginning of the package name is important.
-    It tells `npm` that the `cli` package is located in the `antora` group.
-    If you omit this character, `npm` will assume the package name is the name of a git repository on GitHub.
-    The second `@` offsets the requested version number.footnote:[Clarification about this statement.]
-    Only the major and minor segments are specified to ensure you receive the latest patch update.
-
-    #{(['filler'] * 25).join %(\n\n)}
-    --
-
-    Make it snow.footnote:[dollar bills]
-    EOS
-
-    (expect to_file).to visually_match 'example-unbreakable-option-break.pdf'
-  end
-
   it 'should keep block together if it can fit on one page' do
     pdf = to_pdf <<~EOS, analyze: true
     #{(['filler'] * 15).join %(\n\n)}
---
+++ spec/open_spec.rb
@@ -0,0 +1,54 @@
+require_relative 'spec_helper'
+
+describe 'Asciidoctor::PDF::Converter - Open' do
+  it 'should keep block together when it has the unbreakable option' do
+    to_file = to_pdf_file <<~EOS, 'open-unbreakable-option-fit.pdf'
+    Make it rain.footnote:[money]
+
+    #{(['filler'] * 21).join %(\n\n)}
+    [%unbreakable]
+    --
+    To install Antora, open a terminal and type:
+    
+     $ npm i -g @antora/cli@2.2 @antora/site-generator-default@2.2
+    
+    IMPORTANT: The `@` at the beginning of the package name is important.
+    It tells `npm` that the `cli` package is located in the `antora` group.
+    If you omit this character, `npm` will assume the package name is the name of a git repository on GitHub.
+    The second `@` offsets the requested version number.footnote:[Clarification about this statement.]
+    Only the major and minor segments are specified to ensure you receive the latest patch update.
+    --
+
+    Make it snow.footnote:[dollar bills]
+    EOS
+
+    (expect to_file).to visually_match 'open-unbreakable-option-fit.pdf'
+  end
+
+  it 'should break an unbreakable block if it does not fit on one page' do
+    to_file = to_pdf_file <<~EOS, 'open-unbreakable-option-break.pdf'
+    Make it rain.footnote:[money]
+
+    #{(['filler'] * 21).join %(\n\n)}
+
+    [%unbreakable]
+    --
+    To install Antora, open a terminal and type:
+    
+     $ npm i -g @antora/cli@2.2 @antora/site-generator-default@2.2
+    
+    IMPORTANT: The `@` at the beginning of the package name is important.
+    It tells `npm` that the `cli` package is located in the `antora` group.
+    If you omit this character, `npm` will assume the package name is the name of a git repository on GitHub.
+    The second `@` offsets the requested version number.footnote:[Clarification about this statement.]
+    Only the major and minor segments are specified to ensure you receive the latest patch update.
+
+    #{(['filler'] * 25).join %(\n\n)}
+    --
+
+    Make it snow.footnote:[dollar bills]
+    EOS
+
+    (expect to_file).to visually_match 'open-unbreakable-option-break.pdf'
+  end
+end
