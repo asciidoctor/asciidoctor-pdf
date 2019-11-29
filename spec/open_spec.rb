2c0ea61a01688a66e5ee1488a69104690d9f9cb3
Dan Allen <dan.j.allen@gmail.com>
2019-11-28 21:15:39 -0700

    remove admonition from open block test to exercise logic that prevents duplication of footnotes

--- spec/open_spec.rb
+++ spec/open_spec.rb
@@ -6,13 +6,14 @@ describe 'Asciidoctor::PDF::Converter - Open' do
     Make it rain.footnote:[money]
 
     #{(['filler'] * 21).join %(\n\n)}
+
     [%unbreakable]
     --
     To install Antora, open a terminal and type:
     
      $ npm i -g @antora/cli@2.2 @antora/site-generator-default@2.2
     
-    IMPORTANT: The `@` at the beginning of the package name is important.
+    The `@` at the beginning of the package name is important.
     It tells `npm` that the `cli` package is located in the `antora` group.
     If you omit this character, `npm` will assume the package name is the name of a git repository on GitHub.
     The second `@` offsets the requested version number.footnote:[Clarification about this statement.]
@@ -37,13 +38,13 @@ describe 'Asciidoctor::PDF::Converter - Open' do
     
      $ npm i -g @antora/cli@2.2 @antora/site-generator-default@2.2
     
-    IMPORTANT: The `@` at the beginning of the package name is important.
+    The `@` at the beginning of the package name is important.
     It tells `npm` that the `cli` package is located in the `antora` group.
     If you omit this character, `npm` will assume the package name is the name of a git repository on GitHub.
     The second `@` offsets the requested version number.footnote:[Clarification about this statement.]
     Only the major and minor segments are specified to ensure you receive the latest patch update.
 
-    #{(['filler'] * 25).join %(\n\n)}
+    #{(['filler inside open block'] * 25).join %(\n\n)}
     --
 
     Make it snow.footnote:[dollar bills]
