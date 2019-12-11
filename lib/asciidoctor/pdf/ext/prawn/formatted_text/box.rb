5f130c29dcff7e94c5be0bfaf9329265522a3de2
Dan Allen <dan.j.allen@gmail.com>
2019-12-11 01:20:26 -0500

    resolves #1414 allow theme to set base text decoration width (PR #1422)

--- lib/asciidoctor/pdf/ext/prawn/formatted_text/box.rb
+++ lib/asciidoctor/pdf/ext/prawn/formatted_text/box.rb
@@ -4,7 +4,7 @@ Prawn::Text::Formatted::Box.prepend (Module.new do
   def draw_fragment_overlay_styles fragment
     if (underline = (styles = fragment.styles).include? :underline) || (styles.include? :strikethrough)
       (doc = fragment.document).save_graphics_state do
-        if (text_decoration_width = (fs = fragment.format_state)[:text_decoration_width])
+        if (text_decoration_width = (fs = fragment.format_state)[:text_decoration_width] || doc.text_decoration_width)
           doc.line_width = text_decoration_width
         end
         if (text_decoration_color = fs[:text_decoration_color])
