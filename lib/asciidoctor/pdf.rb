64417aaec466f19a7ab9dacfc6dcbf9086f0c6b2
Dan Allen <dan.j.allen@gmail.com>
2020-01-06 04:01:44 -0700

    fix library load order

--- lib/asciidoctor/pdf.rb
+++ lib/asciidoctor/pdf.rb
@@ -7,10 +7,10 @@ begin
   require 'prawn/gmagick'
 rescue LoadError
 end unless defined? GMagick::Image
-require_relative 'pdf/theme_loader'
 require_relative 'pdf/measurements'
 require_relative 'pdf/sanitizer'
 require_relative 'pdf/temporary_path'
 require_relative 'pdf/text_transformer'
 require_relative 'pdf/ext'
+require_relative 'pdf/theme_loader'
 require_relative 'pdf/converter'
