9829dc69dfdec7025a8356480b4595f0d5c3fea1
Dan Allen <dan.j.allen@gmail.com>
2020-01-07 02:01:02 -0700

    make sidebar border test more robust

--- spec/sidebar_spec.rb
+++ spec/sidebar_spec.rb
@@ -3,29 +3,16 @@
 require_relative 'spec_helper'
 
 describe 'Asciidoctor::PDF::Converter - Sidebar' do
-  it 'should keep sidebar together if it can fit on one page' do
-    pdf = to_pdf <<~EOS, analyze: true
-    #{(['filler'] * 15).join %(\n\n)}
-
-    .Sidebar
-    ****
-    #{(['content'] * 15).join %(\n\n)}
-    ****
-    EOS
-
-    sidebar_text = (pdf.find_text 'Sidebar')[0]
-    (expect sidebar_text[:page_number]).to be 2
-  end
-
   it 'should draw line around sidebar block' do
-    pdf = to_pdf <<~'EOS', analyze: :line
+    pdf = to_pdf <<~'EOS', analyze: :line, pdf_theme: { sidebar_background_color: 'transparent' }
     ****
     sidebar
     ****
     EOS
 
-    # NOTE lines without width are for the background
-    (expect pdf.lines.select {|it| it[:width] }).to have_size 4
+    (expect pdf.lines).to have_size 4
+    (expect pdf.lines.map {|it| it[:color] }.uniq).to eql ['E1E1E1']
+    (expect pdf.lines.map {|it| it[:width] }.uniq).to eql [0.5]
   end
 
   it 'should use block title as heading of sidebar block' do
@@ -45,6 +32,20 @@ describe 'Asciidoctor::PDF::Converter - Sidebar' do
     (expect title_text[:font_size]).to be 13
     (expect title_text[:x]).to be > 100
     (expect title_text[:y]).to be < sidebar_border_top
+  end
+
+  it 'should keep sidebar together if it can fit on one page' do
+    pdf = to_pdf <<~EOS, analyze: true
+    #{(['filler'] * 15).join %(\n\n)}
+
+    .Sidebar
+    ****
+    #{(['content'] * 15).join %(\n\n)}
+    ****
+    EOS
+
+    sidebar_text = (pdf.find_text 'Sidebar')[0]
+    (expect sidebar_text[:page_number]).to be 2
   end
 
   it 'should split block if it cannot fit on one page' do
