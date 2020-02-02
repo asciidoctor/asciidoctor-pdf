36dc014fea3586ff75b5c16213a73d3f8edd8109
Dan Allen <dan.j.allen@gmail.com>
2020-02-01 23:52:59 -0700

    resolves #1533 place dots on correct page when section title in TOC wraps across a page boundary (PR #1534)

--- lib/asciidoctor/pdf/formatted_text/fragment_position_renderer.rb
+++ lib/asciidoctor/pdf/formatted_text/fragment_position_renderer.rb
@@ -2,12 +2,13 @@
 
 module Asciidoctor::PDF::FormattedText
   class FragmentPositionRenderer
-    attr_reader :top, :right, :bottom, :left
+    attr_reader :top, :right, :bottom, :left, :page_number
 
     def render_behind fragment
       @top = fragment.top
       @right = (@left = fragment.left) + fragment.width
       @bottom = fragment.bottom
+      @page_number = fragment.document.page_number
     end
   end
 end
