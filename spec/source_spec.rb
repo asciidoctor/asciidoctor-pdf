a045ddc8fa58447350f0ecea02eb0fdc0b3bbd9e
Dan Allen <dan.j.allen@gmail.com>
2020-05-13 23:52:02 -0600

    test PHP highlighting when source highlighter is Pygments

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -510,6 +510,37 @@ describe 'Asciidoctor::PDF::Converter - Source' do
       (expect lines[2]).to eql %(\u00a0   return 0;)
     end
 
+    it 'should enable start_inline option for PHP by default' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      :source-highlighter: pygments
+
+      [source,php]
+      ----
+      echo "<?php";
+      ----
+      EOS
+
+      echo_text = (pdf.find_text 'echo')[0]
+      (expect echo_text).not_to be_nil
+      # NOTE the echo keyword should be highlighted
+      (expect echo_text[:font_color]).to eql '008800'
+    end
+
+    it 'should not enable the start_inline option for PHP if the mixed option is set' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      :source-highlighter: pygments
+
+      [source%mixed,php]
+      ----
+      echo "<?php";
+      ----
+      EOS
+
+      echo_text = (pdf.find_text 'echo')[0]
+      # NOTE the echo keyword should not be highlighted
+      (expect echo_text).to be_nil
+    end
+
     it 'should not crash when aligning line numbers' do
       (expect do
         expected_lines = <<~'EOS'.split ?\n
