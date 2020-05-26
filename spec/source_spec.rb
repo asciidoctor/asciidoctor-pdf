a98481bce3039d3c0865e576e7e6d2cdaca89083
Dan Allen <dan.j.allen@gmail.com>
2020-05-25 23:39:36 -0600

    add test that converter gracefully ignores client-side source highlighter

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -839,6 +839,24 @@ describe 'Asciidoctor::PDF::Converter - Source' do
     end
   end if (ENV.key? 'PYGMENTS_VERSION') && !(Gem.win_platform? && RUBY_ENGINE == 'jruby')
 
+  context 'Unsupported' do
+    it 'should apply specialcharacters substitution for client-side syntax highlighter' do
+      pdf = to_pdf <<~'EOS', analyze: true
+      :source-highlighter: highlight.js
+
+      [source,xml]
+      ----
+      <root>
+        <child>content</child>
+      </root>
+      ----
+      EOS
+
+      (expect pdf.lines).to eql ['<root>', %(\u00a0 <child>content</child>), '</root>']
+      (expect pdf.text.map {|it| it[:font_color] }.uniq).to eql ['333333']
+    end
+  end
+
   context 'Callouts' do
     it 'should allow callout to be escaped' do
       pdf = to_pdf <<~'EOS', analyze: true
