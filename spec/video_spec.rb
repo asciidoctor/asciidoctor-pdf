f53ba6b3519a223cace3dcdab08ab0c185cd7855
Dan Allen <dan.j.allen@gmail.com>
2020-07-15 03:25:34 -0600

    add fallback assertion for Vimeo poster test if request is rate limited

--- spec/video_spec.rb
+++ spec/video_spec.rb
@@ -43,7 +43,12 @@ describe 'Asciidoctor::PDF::Converter - Video' do
       link_annotation = annotations[0]
       (expect link_annotation[:Subtype]).to be :Link
       (expect link_annotation[:A][:URI]).to eql %(https://vimeo.com/#{video_id})
-      (expect to_file).to visually_match 'video-vimeo-poster.pdf'
+      # NOTE: Vimeo sometimes returns a skeleton XML document, perhaps due to rate limiting
+      if (get_images pdf, 1).size == 1
+        (expect to_file).to visually_match 'video-vimeo-poster.pdf'
+      else
+        (expect (pdf.page 1).text).to eql %(\u25ba\u00a0https://vimeo.com/#{video_id} (Vimeo video))
+      end
     end
   end
 end
