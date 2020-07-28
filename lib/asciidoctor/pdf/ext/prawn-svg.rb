2179697c06f7171c8773337e2df79ad151b8d9ef
Dan Allen <dan.j.allen@gmail.com>
2020-07-27 23:58:48 -0600

    remove outdated patch for prawn-svg

--- lib/asciidoctor/pdf/ext/prawn-svg.rb
+++ lib/asciidoctor/pdf/ext/prawn-svg.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'prawn-svg' unless defined? Prawn::SVG::Interface
-require_relative 'prawn-svg/interface'
 require_relative 'prawn-svg/loaders/web'
 require_relative 'prawn-svg/url_loader'
 # NOTE: disable system fonts since they're non-portable
