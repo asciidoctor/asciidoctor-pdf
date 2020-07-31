22a8f4cbc6467186ae8611f46da1ea585b415c7e
Dan Allen <dan.j.allen@gmail.com>
2020-07-30 23:51:41 -0600

    remove outdated patch to add AbstractBlock#sections? method

--- lib/asciidoctor/pdf/ext/asciidoctor.rb
+++ lib/asciidoctor/pdf/ext/asciidoctor.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 # NOTE: these are either candidates for inclusion in Asciidoctor core or backports
-require_relative 'asciidoctor/abstract_block'
 require_relative 'asciidoctor/section'
 require_relative 'asciidoctor/list'
 require_relative 'asciidoctor/image'
