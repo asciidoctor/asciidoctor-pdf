4952c077d5bf3ad4a6e738ccbcad1996de27fb12
Dan Allen <dan.j.allen@gmail.com>
2020-07-31 23:43:18 -0600

    remove unnecessary conditional in InlineDestinationMarker callback

--- lib/asciidoctor/pdf/formatted_text/inline_destination_marker.rb
+++ lib/asciidoctor/pdf/formatted_text/inline_destination_marker.rb
@@ -6,16 +6,14 @@ module Asciidoctor::PDF::FormattedText
 
     # render_behind is called before the text is printed
     def render_behind fragment
-      unless (pdf = fragment.document).scratch?
-        if (name = fragment.format_state[:name])
-          pdf.index.link_dest_to_page name, pdf.page_number if fragment.format_state[:type] == :indexterm
-          # get precise position of the reference (x, y)
-          dest_rect = fragment.absolute_bounding_box
-          pdf.add_dest name, (pdf.dest_xyz dest_rect[0], dest_rect[-1])
-          # prevent any text from being written
-          fragment.conceal true
-        end
-      end
+      return if (pdf = fragment.document).scratch?
+      name = fragment.format_state[:name]
+      pdf.index.link_dest_to_page name, pdf.page_number if fragment.format_state[:type] == :indexterm
+      # get precise position of the reference (x, y)
+      dest_rect = fragment.absolute_bounding_box
+      pdf.add_dest name, (pdf.dest_xyz dest_rect[0], dest_rect[-1])
+      # prevent any text from being written
+      fragment.conceal true
     end
   end
 end
