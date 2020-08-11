dbd971b03da1906ee746bf605e4b8c0a8f17581d
Dan Allen <dan.j.allen@gmail.com>
2020-08-10 23:59:05 -0600

    remove outdated patches for PDF::Core now provided by Prawn::Template

--- lib/asciidoctor/pdf/ext/pdf-core/page.rb
+++ lib/asciidoctor/pdf/ext/pdf-core/page.rb
@@ -3,31 +3,6 @@
 class PDF::Core::Page
   InitialPageContent = %(q\n)
 
-  # Restore the new_content_stream method from PDF::Core::Page
-  #
-  # The prawn-templates gem relies on the new_content_stream method on
-  # PDF::Core::Page, which was removed in pdf-core 0.3.1. prawn-templates is
-  # used for importing a single-page PDF into the current document.
-  #
-  # see https://github.com/prawnpdf/pdf-core/commit/67f9a08a03bcfcc5a24cf76b135c218d3d3ab05d
-  def new_content_stream
-    return if in_stamp_stream?
-    dictionary.data[:Contents] = [content] unless Array === dictionary.data[:Contents]
-    @content = document.ref({})
-    dictionary.data[:Contents] << document.state.store[@content]
-    document.open_graphics_state
-  end unless method_defined? :new_content_stream
-
-  # NOTE: alias method to avoid warning if another gem replaces this method
-  alias __new_content_stream new_content_stream
-
-  # Restore the imported_page? method from PDF::Core::Page
-  #
-  # see https://github.com/prawnpdf/pdf-core/commit/0e326a838e142061be8e062168190fae6b3b1dcf
-  def imported_page?
-    @imported_page
-  end unless method_defined? :imported_page?
-
   # Record the page's current state as the tare content stream (i.e., empty, meaning no content has been written).
   def tare_content_stream
     @tare_content_stream = content.stream.filtered_stream
