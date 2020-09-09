83401d739683ce81a686209fbcb3105686e74c38
Dan Allen <dan.j.allen@gmail.com>
2020-09-09 00:55:42 -0600

    resolves #1764 take hard line breaks into account when computing natural width of table cell

--- lib/asciidoctor/pdf/ext/prawn-table/cell/text.rb
+++ lib/asciidoctor/pdf/ext/prawn-table/cell/text.rb
@@ -16,4 +16,16 @@ class Prawn::Table::Cell::Text
       logger.error %(the table cell on page #{@pdf.page_number} has been truncated; Asciidoctor PDF does not support table cell content that exceeds the height of a single page) unless remaining_text.empty? || @pdf.scratch?
     end
   end
+
+  # Override the styled_width_of to account for hard line breaks
+  remove_method :styled_width_of
+  def styled_width_of text
+    # NOTE: remove :style since it's handled by with_font
+    options = @text_options.reject {|k| k == :style }
+    if text.length > 3 && (text.include? '<br>')
+      (text.split '<br>').map {|line| (line = line.strip).empty? ? 0 : with_font { @pdf.width_of line, options } }.max
+    else
+      with_font { @pdf.width_of text, options }
+    end
+  end
 end
