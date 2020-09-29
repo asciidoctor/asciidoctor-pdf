5fd09c0630400fee72d65733385e5e108d142568
Dan Allen <dan.j.allen@gmail.com>
2020-09-28 23:57:02 -0600

    consolidate logic in source wrap; add notes

--- lib/asciidoctor/pdf/formatted_text/source_wrap.rb
+++ lib/asciidoctor/pdf/formatted_text/source_wrap.rb
@@ -10,17 +10,15 @@ module Asciidoctor
         # Note that this implementation assumes that the :single_line option is falsy.
         def wrap array
           initialize_wrap array
-          stop = nil
-          highlight_line = nil
+          highlight_line = stop = nil
           unconsumed = @arranger.unconsumed
-          if (linenum_fragment = unconsumed[0] || {})[:linenum]
-            linenum_spacer = { text: NoBreakSpace + (' ' * (linenum_fragment[:text].length - 1)) }
-          end
           until stop
-            if linenum_spacer && (first_fragment = unconsumed[0])
+            if (first_fragment = unconsumed[0])
               if first_fragment[:linenum]
+                linenum_spacer ||= { text: NoBreakSpace + (' ' * (first_fragment[:text].length - 1)) }
                 highlight_line = (second_fragment = unconsumed[1]) && (second_fragment[:highlight]) ? second_fragment.dup : nil
               else
+                # NOTE: a wrapped line
                 first_fragment[:text] = first_fragment[:text].lstrip
                 @arranger.unconsumed.unshift highlight_line if highlight_line
                 @arranger.unconsumed.unshift linenum_spacer.dup
