bd6da4788e7bfef964bf2c35c01ed14c3ae8e369
Dan Allen <dan.j.allen@gmail.com>
2020-09-30 23:59:15 -0600

    detect and load optimizer class in init_pdf

--- lib/asciidoctor/pdf/converter.rb
+++ lib/asciidoctor/pdf/converter.rb
@@ -427,7 +427,12 @@ module Asciidoctor
         @index ||= IndexCatalog.new
         # NOTE: we have to init Pdfmark class here while we have reference to the doc
         @pdfmark = (doc.attr? 'pdfmark') ? (Pdfmark.new doc) : nil
-        @optimize = doc.attr 'optimize'
+        # NOTE: defer instantiating optimizer until we know min pdf version
+        if (@optimize = (doc.attr 'optimize'))
+          unless (defined? ::Asciidoctor::PDF::Optimizer) || !(Helpers.require_library OptimizerRequirePath, 'rghost', :warn).nil?
+            @optimize = nil
+          end
+        end
         init_scratch_prototype
         self
       end
@@ -3717,7 +3722,7 @@ module Asciidoctor
           pdf_doc.render_file target
           # QUESTION: restore attributes first?
           @pdfmark&.generate_file target
-          (Optimizer.new @optimize, pdf_doc.min_version).generate_file target if @optimize && ((defined? ::Asciidoctor::PDF::Optimizer) || !(Helpers.require_library OptimizerRequirePath, 'rghost', :warn).nil?)
+          (Optimizer.new @optimize, pdf_doc.min_version).generate_file target if @optimize
           to_file = true
         end
         if !ENV['KEEP_ARTIFACTS']
