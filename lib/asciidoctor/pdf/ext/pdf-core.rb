bef9a1f93eba06b73c67d7d28202e8f19dd34bc2
Dan Allen <dan.j.allen@gmail.com>
2020-11-13 23:43:18 -0700

    add note about when real_params override is needed [skip ci]

--- lib/asciidoctor/pdf/ext/pdf-core.rb
+++ lib/asciidoctor/pdf/ext/pdf-core.rb
@@ -9,6 +9,8 @@ module PDF::Core
 
     alias _initial_real_params real_params
     def real_params array
+      # NOTE: real_params is used by transformation_matrix in prawn > 2.3.0
+      # see https://github.com/prawnpdf/prawn/commit/1dd11695351c0f1c4f4aacb6547e552bd1b98196
       return array.map {|it| it.to_f.round 5 }.join ' ' if (caller_locations 1, 1)[0].base_label == 'transformation_matrix'
       _initial_real_params array
     end
