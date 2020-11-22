dfb5ba342bdba457129e7b2c3d7aec8ad52cc225
Dan Allen <dan.j.allen@gmail.com>
2020-11-22 03:03:56 -0700

    use Pathname#absolute? as the fallback for File.absolute_path?

--- lib/asciidoctor/pdf/ext/core/file.rb
+++ lib/asciidoctor/pdf/ext/core/file.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class File
-  class << self
-    def absolute_path? path
-      (path.start_with? '/') || (ALT_SEPARATOR && (path.start_with? (absolute_path path).slice 0, 3))
-    end unless method_defined? :absolute_path?
-  end
+  # NOTE: remove once minimum required Ruby version is at least 2.7
+  def self.absolute_path? path
+    (::Pathname.new path).absolute?
+  end unless respond_to? :absolute_path?
 end
