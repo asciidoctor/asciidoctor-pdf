cb5211e984da57a65c26bfce9c2b0ddf7f40b018
Dan Allen <dan.j.allen@gmail.com>
2020-11-30 03:09:10 -0700

    fix test

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -27,7 +27,7 @@ describe 'Asciidoctor::PDF::Converter - Table' do
     end).to not_raise_exception & (log_message severity: :WARN, message: 'no rows found in table')
   end
 
-  it 'wip should not crash when rows have cells with colspans of varying length' do
+  it 'should not crash when rows have cells with colspans of varying length' do
     (expect do
       pdf = to_pdf <<~'EOS', analyze: true
       [cols=3*]
@@ -39,8 +39,8 @@ describe 'Asciidoctor::PDF::Converter - Table' do
       |===
       EOS
 
-      y_text = pdf.find_unique_text 'Y'
-      z_text = pdf.find_unique_text 'Z'
+      y_text = (pdf.find_text 'Y')[0]
+      z_text = (pdf.find_text 'Z')[0]
       (expect y_text[:y]).to eql z_text[:y]
       (expect y_text[:x]).to be < z_text[:x]
     end).to not_raise_exception
