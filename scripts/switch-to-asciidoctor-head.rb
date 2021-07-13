c4872a6b8350d6a005bc1051d1e8e3be764769fe
Dan Allen <dan.j.allen@gmail.com>
2021-07-12 23:54:24 -0600

    use branch specified in payload when CI job is triggered by upstream dispatch [skip ci]

--- scripts/switch-to-asciidoctor-head.rb
+++ scripts/switch-to-asciidoctor-head.rb
@@ -1,9 +1,10 @@
 require 'fileutils'
 
+branch = ARGV[0] || 'master'
 # NOTE it's necessary to hot patch the installed gem so that RubyGems can find it without Bundler
 asciidoctor_spec = Gem::Specification.find_by_name 'asciidoctor'
 FileUtils.rm_r asciidoctor_spec.gem_dir, secure: true if Dir.exist? asciidoctor_spec.gem_dir
-Process.wait Process.spawn %(git clone --depth=1 https://github.com/asciidoctor/asciidoctor #{File.basename asciidoctor_spec.gem_dir}), chdir: asciidoctor_spec.gems_dir
+Process.wait Process.spawn %(git clone -b #{branch} --depth=1 https://github.com/asciidoctor/asciidoctor #{File.basename asciidoctor_spec.gem_dir}), chdir: asciidoctor_spec.gems_dir
 
 Dir.chdir asciidoctor_spec.gem_dir do
   new_asciidoctor_spec_contents = File.readlines 'asciidoctor.gemspec', mode: 'r:UTF-8'
