c0cc916fb949e3b868f5f9f293d409a08738b110
Dan Allen <dan.j.allen@gmail.com>
2022-01-19 03:30:28 -0700

    fix warning

--- lib/asciidoctor/pdf/ext/prawn/formatted_text/arranger.rb
+++ lib/asciidoctor/pdf/ext/prawn/formatted_text/arranger.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+module Prawn::Text::NoopLstripBang
+  def lstrip!; end
+end
+
 Prawn::Text::Formatted::Arranger.prepend (Module.new do
   def initialize *_args
     super
@@ -19,10 +23,7 @@ Prawn::Text::Formatted::Arranger.prepend (Module.new do
   end
 
   def next_string
-    if (string = super) == @dummy_text
-      def string.lstrip!; end
-    end
-    string
+    (string = super) == @dummy_text ? (string.extend Prawn::Text::NoopLstripBang) : string
   end
 
   def apply_font_size size, styles
