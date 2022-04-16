1e39955307165389d3791c35bb31cdf43b076a46
Dan Allen <dan.j.allen@gmail.com>
2022-04-16 03:45:12 -0600

    bump minimum Ruby version to 2.7 and remove obsolete patches (PR #2038)

--- lib/asciidoctor/pdf/ext/core.rb
+++ lib/asciidoctor/pdf/ext/core.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'core/object'
 require_relative 'core/file'
+require_relative 'core/object'
 require_relative 'core/string'
 require_relative 'core/quantifiable_stdout'
