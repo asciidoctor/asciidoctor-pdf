8df430947a27f0a7447ed54df06be3a93bc603f3
Dan Allen <dan.j.allen@gmail.com>
2022-05-16 04:33:07 -0600

    remove superfluous args on ColumnBox#move_past_bottom

--- lib/asciidoctor/pdf/ext/prawn/document/column_box.rb
+++ lib/asciidoctor/pdf/ext/prawn/document/column_box.rb
@@ -5,7 +5,7 @@ Prawn::Document::ColumnBox.prepend (Module.new do
     stretchy? ? @parent.absolute_bottom : super
   end
 
-  def move_past_bottom *_args
+  def move_past_bottom
     initial_page = @document.page
     super
     if (page = @document.page) != initial_page && page.margins != initial_page.margins
