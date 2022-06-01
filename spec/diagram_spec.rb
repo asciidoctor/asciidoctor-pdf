62d47ee286a209fb9a302f20a276d51a93221ecd
Dan Allen <dan.j.allen@gmail.com>
2022-06-01 03:12:46 -0600

    add integration test with Asciidoctor Kroki

--- spec/diagram_spec.rb
+++ spec/diagram_spec.rb
@@ -34,3 +34,43 @@ describe 'Asciidoctor Diagram Integration', if: (gem_available? 'asciidoctor-dia
     (expect pdfmark_file).to exist
   end
 end)
+
+describe 'Asciidoctor Kroki Integration', if: (gem_available? 'asciidoctor-kroki'), &(proc do
+  # NOTE: asciidoctor-kroki not honoring :to_dir option; see https://github.com/Mogztter/asciidoctor-kroki/issues/371
+  it 'should locate generated diagram in output directory' do
+    require 'asciidoctor-kroki'
+    input_file = Pathname.new fixture_file 'diagrams.adoc'
+    attributes = {
+      'sequence-diagram-name' => 'sequence-diagram-d',
+      'kroki-fetch-diagram' => '',
+      # imagesdir and imagesoutdir required until fixes are applied to Asciidoctor Kroki
+      'imagesdir' => output_dir,
+      'imagesoutdir' => output_dir,
+    }
+    pdf = to_pdf input_file, safe: :unsafe, attributes: attributes, analyze: :image
+    (expect pdf.images).to have_size 1
+    (expect pdf.images[0][:data].length).to be > 5000
+    diagram_file = Dir[File.join output_dir, '*.png'][0]
+    (expect diagram_file).not_to be_nil
+  end
+
+  # NOTE: asciidoctor-kroki not honoring :to_dir option; see https://github.com/Mogztter/asciidoctor-kroki/issues/371
+  it 'should overwrite generated diagram file on subsequent invocations' do
+    require 'asciidoctor-kroki'
+    input_file = Pathname.new fixture_file 'diagrams.adoc'
+    attributes = {
+      'sequence-diagram-name' => 'sequence-diagram-e',
+      'kroki-fetch-diagram' => '',
+      # imagesdir and imagesoutdir required until fixes are applied to Asciidoctor Kroki
+      'imagesdir' => output_dir,
+      'imagesoutdir' => output_dir,
+    }
+    2.times do
+      pdf = to_pdf input_file, safe: :unsafe, attributes: attributes, analyze: :image
+      (expect pdf.images).to have_size 1
+      (expect pdf.images[0][:data].length).to be > 5000
+      diagram_file = Dir[File.join output_dir, '*.png'][0]
+      (expect diagram_file).not_to be_nil
+    end
+  end
+end)
