f3b1c79b1672c06b5d4c4dfae0a8fb09cfad9ac5
Dan Allen <dan.j.allen@gmail.com>
2022-06-02 05:04:13 -0600

    resolves #1941 allow image path in SVG to refer to any location within Asciidoctor jail (PR #2227)

---
+++ lib/asciidoctor/pdf/ext/prawn-svg/loaders/file.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+Prawn::SVG::Loaders::File.prepend (Module.new do
+  attr_reader :jail_path
+
+  def initialize root_path
+    if Hash === root_path
+      @jail_path = root_path[:jail]
+      root_path = root_path[:root]
+      super
+    else
+      super
+      @jail_path = self.root_path
+    end
+  end
+
+  def assert_valid_path! path
+    if jail_path && !(path.start_with? %(#{jail_path}#{File::SEPARATOR}))
+      raise Prawn::SVG::UrlLoader::Error, %(file path points to location outside of jail #{jail_path})
+    end
+  end
+end)
