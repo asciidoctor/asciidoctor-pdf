c6b9e9b92847541004316f5ea6b89c54cf75d735
Dan Allen <dan.j.allen@gmail.com>
2022-06-06 01:33:15 -0600

    don't apply column box in multiple column example if already in column box

--- docs/modules/extend/examples/pdf-converter-columns.rb
+++ docs/modules/extend/examples/pdf-converter-columns.rb
@@ -2,8 +2,16 @@ class PDFConverterColumns < (Asciidoctor::Converter.for 'pdf')
   register_for 'pdf'
 
   def traverse node
-    return super unless node.context == :document && (columns = theme.base_columns)
-    column_box [0, cursor], columns: columns, width: bounds.width, reflow_margins: true, spacer: theme.base_column_gap do
+    if node.context == :document &&
+        (columns = ColumnBox === bounds ? 1 : theme.base_columns || 1) > 1
+      column_box [bounds.left, cursor],
+        columns: columns,
+        width: bounds.width,
+        reflow_margins: true,
+        spacer: theme.base_column_gap do
+        super
+      end
+    else
       super
     end
   end
