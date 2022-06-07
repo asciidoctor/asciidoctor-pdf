d5bde5d2e1feb9d719cd1e2e28b95f0c65c3a1f9
Dan Allen <dan.j.allen@gmail.com>
2022-06-07 00:37:03 -0600

    resolves #2230 correctly compute value of to cursor on extent when column box starts below top of page (PR #2231)

--- lib/asciidoctor/pdf/ext/prawn/document/column_box.rb
+++ lib/asciidoctor/pdf/ext/prawn/document/column_box.rb
@@ -9,12 +9,12 @@ Prawn::Document::ColumnBox.prepend (Module.new do
     (doc = @document).y = @y
     return if (@current_column = (@current_column + 1) % @columns) > 0
     par = @parent
-    @y = par.absolute_top if @reflow_margins
+    @y = par.absolute_top if (reset_y = @reflow_margins) && (reset_y == true || reset_y > doc.page_number)
     initial_margins = doc.page.margins
     par.move_past_bottom
     if doc.page.margins != initial_margins
       doc.bounds = self.class.new doc, par, (margin_box = doc.margin_box).absolute_top_left,
-        columns: @columns, reflow_margins: true, spacer: @spacer, width: margin_box.width
+        columns: @columns, reflow_margins: @reflow_margins, spacer: @spacer, width: margin_box.width
     end
     nil
   end
