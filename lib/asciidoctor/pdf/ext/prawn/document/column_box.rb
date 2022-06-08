f574295075d701ba4b2c3eee2706b3b2f1220247
Dan Allen <dan.j.allen@gmail.com>
2022-06-08 01:10:42 -0600

    encapsulate logic to adjust column box inside float and dry run
    - add ColumnBox#single_file method
    - add accessor for current_column

--- lib/asciidoctor/pdf/ext/prawn/document/column_box.rb
+++ lib/asciidoctor/pdf/ext/prawn/document/column_box.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Prawn::Document::ColumnBox.prepend (Module.new do
+  attr_accessor :current_column
+
   def absolute_bottom
     stretchy? ? @parent.absolute_bottom : super
   end
@@ -16,6 +18,19 @@ Prawn::Document::ColumnBox.prepend (Module.new do
       doc.bounds = self.class.new doc, par, (margin_box = doc.margin_box).absolute_top_left,
         columns: @columns, reflow_margins: @reflow_margins, spacer: @spacer, width: margin_box.width
     end
+    nil
+  end
+
+  # Rearranges the column box into a single column, where the original columns are in a single file. Used
+  # for the purpose of computing the extent of content in a scratch document.
+  def single_file
+    if @parent.absolute_top > @y && @columns > @current_column + 1
+      # defer reflow margins until all columns on current page have been exhausted
+      @reflow_margins = @document.page_number + (@columns - @current_column)
+    end
+    @width = bare_column_width
+    @columns = 1
+    @current_column = 0
     nil
   end
 end)
