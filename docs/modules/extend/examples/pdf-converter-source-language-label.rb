0f9c6401c6da4a26bf312f42dac66f8d74860f62
Dan Allen <dan.j.allen@gmail.com>
2022-06-11 00:38:20 -0600

    add extended converter example that imprints a source language label on code blocks

---
+++ docs/modules/extend/examples/pdf-converter-source-language-label.rb
@@ -0,0 +1,26 @@
+class PDFConverterSourceLanguageLabel < (Asciidoctor::Converter.for 'pdf')
+  register_for 'pdf'
+
+  def arrange_block node, &block
+    return super unless node.style == 'source' && (lang = node.attr 'language')
+    super node do |extent|
+      return_val = instance_exec extent, &block
+      if extent && !scratch?
+        float do
+          go_to_page extent.from.page
+          move_cursor_to extent.from.cursor
+          pad_box theme.code_padding, node do
+            theme_font :code do
+              ink_prose lang,
+                align: :right,
+                text_transform: :uppercase,
+                margin: 0,
+                color: theme.quote_cite_font_color
+            end
+          end
+        end
+      end
+      return_val
+    end
+  end
+end
