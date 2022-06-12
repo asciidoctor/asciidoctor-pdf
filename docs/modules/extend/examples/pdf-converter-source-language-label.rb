bbd81b1086cf4942c292e3f98da58e3c19a78b81
Dan Allen <dan.j.allen@gmail.com>
2022-06-12 00:49:58 -0600

    take column into account in extended converter that imprints source language label

--- docs/modules/extend/examples/pdf-converter-source-language-label.rb
+++ docs/modules/extend/examples/pdf-converter-source-language-label.rb
@@ -4,10 +4,13 @@ class PDFConverterSourceLanguageLabel < (Asciidoctor::Converter.for 'pdf')
   def arrange_block node, &block
     return super unless node.style == 'source' && (lang = node.attr 'language')
     super node do |extent|
+      current_column = bounds.current_column if ColumnBox === bounds
       return_val = instance_exec extent, &block
       if extent && !scratch?
         float do
-          go_to_page extent.from.page
+          go_to_page extent.current.page
+          bounds.current_column = current_column if ColumnBox === bounds
+          (extent.from.page - extent.current.page).times { advance_page }
           move_cursor_to extent.from.cursor
           pad_box theme.code_padding, node do
             theme_font :code do
