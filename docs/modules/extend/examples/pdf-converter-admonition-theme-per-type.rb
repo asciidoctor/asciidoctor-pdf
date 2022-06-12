a9d2b9342c94d2339e65ab27fe3014925b283ac1
Dan Allen <dan.j.allen@gmail.com>
2022-06-12 01:20:51 -0600

    add example of extended converter to theme specific admonition types

---
+++ docs/modules/extend/examples/pdf-converter-admonition-theme-per-type.rb
@@ -0,0 +1,16 @@
+class PDFConverterAdmonitionThemePerType < (Asciidoctor::Converter.for 'pdf')
+  register_for 'pdf'
+
+  def convert_admonition node
+    type = node.attr 'name'
+    key_prefix = %(admonition_#{type}_)
+    entries = theme.each_pair.select {|name, val| name.start_with? key_prefix }
+    return super entries.empty?
+    save_theme do
+      entries.each do |name, val|
+        theme[%(admonition_#{name.to_s.delete_prefix key_prefix})] = val
+      end
+      super
+    end
+  end
+end
