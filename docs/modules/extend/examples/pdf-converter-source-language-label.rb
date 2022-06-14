4724bed5d9ceb818acad28302ea92a3fccfee527
Dan Allen <dan.j.allen@gmail.com>
2022-06-13 23:55:40 -0600

    store zero-based column of current and from on Extent (PR #2244)

--- docs/modules/extend/examples/pdf-converter-source-language-label.rb
+++ docs/modules/extend/examples/pdf-converter-source-language-label.rb
@@ -4,13 +4,11 @@ class PDFConverterSourceLanguageLabel < (Asciidoctor::Converter.for 'pdf')
   def arrange_block node, &block
     return super unless node.style == 'source' && (lang = node.attr 'language')
     super node do |extent|
-      current_column = bounds.current_column if ColumnBox === bounds
       return_val = instance_exec extent, &block
       if extent && !scratch?
         float do
-          go_to_page extent.current.page
-          bounds.current_column = current_column if ColumnBox === bounds
-          (extent.from.page - extent.current.page).times { advance_page }
+          go_to_page extent.from.page
+          bounds.current_column = extent.from.column if ColumnBox === bounds
           move_cursor_to extent.from.cursor
           pad_box theme.code_padding, node do
             theme_font :code do
