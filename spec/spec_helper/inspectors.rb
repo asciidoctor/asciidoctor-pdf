9b3dafd9aa0ee5d692e1829be7da7b4c57d5a26b
Dan Allen <dan.j.allen@gmail.com>
2022-07-15 23:59:50 -0600

    remove unused code in test suite [no ci]

--- spec/spec_helper/inspectors.rb
+++ spec/spec_helper/inspectors.rb
@@ -6,17 +6,6 @@ require_relative 'inspectors/line'
 require_relative 'inspectors/rect'
 require_relative 'inspectors/text'
 
-#PDF::Inspector::Text.prepend (Module.new do
-#  def page= page
-#    @page_number = page.number
-#    super
-#  end
-#
-#  def move_text_position tx, ty
-#    @positions << [tx, ty, @page_number]
-#  end
-#end)
-
 (PDF_INSPECTOR_CLASS = {
   image: ImageInspector,
   line: LineInspector,
