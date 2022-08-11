92d71818d9c04750a0329b0601a0ac2f56478a4b
Dan Allen <dan.j.allen@gmail.com>
2022-08-10 23:58:07 -0600

    fix typo in extended converter for admonition theme per type

--- docs/modules/extend/examples/pdf-converter-admonition-theme-per-type.rb
+++ docs/modules/extend/examples/pdf-converter-admonition-theme-per-type.rb
@@ -5,7 +5,7 @@ class PDFConverterAdmonitionThemePerType < (Asciidoctor::Converter.for 'pdf')
     type = node.attr 'name'
     key_prefix = %(admonition_#{type}_)
     entries = theme.each_pair.select {|name, val| name.to_s.start_with? key_prefix }
-    return super entries.empty?
+    return super if entries.empty?
     save_theme do
       entries.each do |name, val|
         theme[%(admonition_#{name.to_s.delete_prefix key_prefix})] = val
