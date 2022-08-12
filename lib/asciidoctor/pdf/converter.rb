55a2d6d4d54d262ac983a7c412a20e6e9ea4a139
Dan Allen <dan.j.allen@gmail.com>
2022-08-11 23:49:10 -0600

    add question about use of bottom gutter logic

--- lib/asciidoctor/pdf/converter.rb
+++ lib/asciidoctor/pdf/converter.rb
@@ -817,6 +817,7 @@ module Asciidoctor
                 if IndentableTextAlignment[prose_opts[:align]] && (text_indent = @theme.prose_text_indent) > 0
                   prose_opts[:indent_paragraphs] = text_indent
                 end
+                # Q: don't we need bottom gutter logic here?
                 ink_prose string, prose_opts
               end
             end
