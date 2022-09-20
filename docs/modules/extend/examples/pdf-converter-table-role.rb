54050e9bde0db741317425f4974b96d257cc7189
Dan Allen <dan.j.allen@gmail.com>
2022-09-19 23:58:40 -0600

    simplify logic in extended converter that supports role on table

--- docs/modules/extend/examples/pdf-converter-table-role.rb
+++ docs/modules/extend/examples/pdf-converter-table-role.rb
@@ -2,18 +2,15 @@ class PDFConverterTableRole < (Asciidoctor::Converter.for 'pdf')
   register_for 'pdf'
 
   def convert_table node
-    if node.role?
-      key_prefix = %(role_<table>_#{node.roles[0]}_)
-      unless (role_entries = theme.each_pair.select {|name, val| name.to_s.start_with? key_prefix }).empty?
-        save_theme do
-          role_entries.each do |name, val|
-            theme[%(table_#{name.to_s.delete_prefix key_prefix})] = val
-          end
-          super
-        end
-        return
+    return super unless node.role?
+    key_prefix = %(role_<table>_#{node.roles[0]}_)
+    role_entries = theme.each_pair.select {|name, val| name.to_s.start_with? key_prefix }
+    return super if role_entries.empty?
+    save_theme do
+      role_entries.each do |name, val|
+        theme[%(table_#{name.to_s.delete_prefix key_prefix})] = val
       end
+      super
     end
-    super
   end
 end
