1783070dc7b39ebc4ef6f320ff16f20ad678c9ec
Dan Allen <dan.j.allen@gmail.com>
2022-10-20 20:26:20 -0600

    import improvements to release scripts from asciidoctor project [no ci]

--- release.sh
+++ release.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-if [ -z $RELEASE_RUBYGEMS_API_KEY ]; then
+# required packages (for ubuntu:kinetic): curl git jq ruby
+
+if [ -z "$RELEASE_RUBYGEMS_API_KEY" ]; then
   echo No API key specified for publishing to rubygems.org. Stopping release.
   exit 1
 fi
-RELEASE_BRANCH=$GITHUB_REF_NAME
-if [ -z $RELEASE_USER ]; then
+export RELEASE_BRANCH=${GITHUB_REF_NAME:-main}
+if [ ! -v RELEASE_USER ]; then
   export RELEASE_USER=$GITHUB_ACTOR
 fi
 RELEASE_GIT_NAME=$(curl -s https://api.github.com/users/$RELEASE_USER | jq -r .name)
 RELEASE_GIT_EMAIL=$RELEASE_USER@users.noreply.github.com
 GEMSPEC=$(ls -1 *.gemspec | head -1)
 RELEASE_NAME=$(ruby -e "print (Gem::Specification.load '$GEMSPEC').name")
-# RELEASE_VERSION must be an exact version number or else it defaults to the next patch release
-if [ -z $RELEASE_VERSION ]; then
+# RELEASE_VERSION must be an exact version number; if not set, defaults to next patch release
+if [ -z "$RELEASE_VERSION" ]; then
   export RELEASE_VERSION=$(ruby -e "print (Gem::Specification.load '$GEMSPEC').version.then { _1.prerelease? ? _1.release.to_s : (_1.segments.tap {|s| s[-1] += 1 }.join ?.) }")
 fi
 
@@ -22,7 +24,7 @@ git config --local user.name "$RELEASE_GIT_NAME"
 git config --local user.email "$RELEASE_GIT_EMAIL"
 
 # configure gem command for publishing
-mkdir $HOME/.gem
+mkdir -p $HOME/.gem
 echo -e "---\n:rubygems_api_key: $RELEASE_RUBYGEMS_API_KEY" > $HOME/.gem/credentials
 chmod 600 $HOME/.gem/credentials
 
@@ -30,14 +32,14 @@ chmod 600 $HOME/.gem/credentials
 (
   set -e
   ruby tasks/version.rb
-  git commit -a -m "release $RELEASE_VERSION [no ci]"
+  git commit -a -m "release $RELEASE_VERSION"
   git tag -m "version $RELEASE_VERSION" v$RELEASE_VERSION
-  RUBYOPT='-r ./gem-version-patch.rb' gem build $GEMSPEC
+  mkdir -p pkg
+  RUBYOPT='-r ./gem-version-patch.rb' gem build $GEMSPEC -o pkg/$RELEASE_NAME-$RELEASE_VERSION.gem
   git push origin $(git describe --tags --exact-match)
-  gem push $RELEASE_NAME-$RELEASE_VERSION.gem
-  git push origin $RELEASE_BRANCH
+  gem push pkg/$RELEASE_NAME-$RELEASE_VERSION.gem
   ruby tasks/release-notes.rb
-  gh release create v$RELEASE_VERSION -t v$RELEASE_VERSION -F release-notes.md -d
+  gh release create v$RELEASE_VERSION -t v$RELEASE_VERSION -F pkg/release-notes.md -d
   ruby tasks/postversion.rb
   git commit -a -m 'prepare branch for development [no ci]'
   git push origin $RELEASE_BRANCH
