2ee246cb1f8bf3d1f664aa7fa53f82f32282a2bd
Dan Allen <dan.j.allen@gmail.com>
2022-10-20 20:39:05 -0600

    remove gem version patch script and update release script

--- Rakefile
+++ Rakefile
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'gem-version-patch'
-
 $default_tasks = [] # rubocop:disable Style/GlobalVars
 Dir.glob('tasks/*.rake').each {|file| load file }
 task default: $default_tasks unless $default_tasks.empty? # rubocop:disable Style/GlobalVars
--- tasks/version.rb
+++ tasks/version.rb
@@ -3,6 +3,7 @@
 require 'time'
 
 release_version = ENV['RELEASE_VERSION']
+release_gem_version = ENV['RELEASE_GEM_VERSION']
 prerelease = (release_version.count '[a-z]') > 0 ? %(-#{(release_version.split '.', 3)[-1]}) : nil
 release_date = Time.now.strftime '%Y-%m-%d'
 release_user = ENV['RELEASE_USER']
@@ -13,7 +14,7 @@ changelog_file = 'CHANGELOG.adoc'
 antora_file = 'docs/antora.yml'
 
 version_contents = (File.readlines version_file, mode: 'r:UTF-8').map do |l|
-  (l.include? 'VERSION') ? (l.sub %r/'[^']+'/, %('#{release_version}')) : l
+  (l.include? 'VERSION') ? (l.sub %r/'[^']+'/, %('#{release_gem_version}')) : l
 end
 
 readme_contents = File.readlines readme_file, mode: 'r:UTF-8'
