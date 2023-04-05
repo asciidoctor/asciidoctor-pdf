f9c5f021c8c8cc935b28e7c4f53dc266d3622b1e
Dan Allen <dan.j.allen@gmail.com>
2023-04-04 23:57:40 -0600

    make URL check in patched File.absolute_path? more robust

--- lib/asciidoctor/pdf/ext/core/file.rb
+++ lib/asciidoctor/pdf/ext/core/file.rb
@@ -3,6 +3,6 @@
 File.singleton_class.prepend (Module.new do
   # NOTE: JRuby < 9.4 doesn't implement this method; JRuby 9.4 implements it incorrectly
   def absolute_path? path
-    (::Pathname.new path).absolute? && ((path.chr == '/' && File::SEPARATOR == '/') || !(URI.split path)[2])
+    (::Pathname.new path).absolute? && !(%r/\A[[:alpha:]][[:alnum:]\-+]*:\/\/\S/.match? path)
   end
 end) if RUBY_ENGINE == 'jruby'
