8da14e7116c1c02b895283aef7e73d0dd9d527d9
Dan Allen <dan.j.allen@gmail.com>
2023-04-10 03:24:55 -0600

    add workaround for JRuby on Windows when value of imagesdir attribute is absolute and contains non-ASCII characters

--- lib/asciidoctor/pdf/ext/core/file.rb
+++ lib/asciidoctor/pdf/ext/core/file.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 File.singleton_class.prepend (Module.new do
+  # NOTE: see https://github.com/jruby/jruby/issues/7750
+  def absolute_path path, dir = nil
+    return super unless dir && !(absolute_path? path)
+    super File.join dir, path
+  end
+
   # NOTE: JRuby < 9.4 doesn't implement this method; JRuby 9.4 implements it incorrectly
   def absolute_path? path
     (::Pathname.new path).absolute? && !(%r/\A[[:alpha:]][[:alnum:]\-+]*:\/\/\S/.match? path)
