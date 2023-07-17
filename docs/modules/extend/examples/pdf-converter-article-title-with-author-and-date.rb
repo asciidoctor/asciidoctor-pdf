526a2d2ace928db3d6e1105db939c19e8627d095
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2023-07-17 20:49:37 +0200

    document how to show author etc. without title page
    Add a use case example that makes author, revdate and revremark visible
    in document types (e.g. articles) without a title page,
    by hooking into the converter function for the document title
    and adding these infos in a line below it.
    
    Workaround for #1883

---
+++ docs/modules/extend/examples/pdf-converter-article-title-with-author-and-date.rb
@@ -0,0 +1,19 @@
+class PDFConverterArticleTitleWithAuthorAndDate < (Asciidoctor::Converter.for 'pdf')
+  register_for 'pdf'
+
+  def ink_general_heading doc, title, opts
+    return super unless opts[:role] == :doctitle # <1>
+    theme_font :heading_doctitle do
+      ink_prose title, align: :center # <2>
+    end
+    revremark = doc.attributes['revremark'] # <3>
+    if doc.author or doc.revdate or revremark # <4>
+      theme_font :base do
+        author_date_separator = (doc.author and doc.revdate) ? " – " : "" # <5>
+        revremark_separator = ((doc.author or doc.revdate) and revremark) ? " | " : "" # <6>
+        ink_prose "#{doc.author}#{author_date_separator}#{doc.revdate}#{revremark_separator}#{revremark}", align: :center # <7>
+      end
+    end
+    theme_margin :heading_doctitle, :bottom
+  end
+end
