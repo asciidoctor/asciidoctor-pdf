4446c2035c9fbf8e5258eba4ddbbc2ed2fbc74d7
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2023-10-07 13:46:11 +0200

    respect document title theming
    (reuse the built-in heading_h1 category as per https://github.com/asciidoctor/asciidoctor-pdf/pull/2439#issuecomment-1748391437)

--- docs/modules/extend/examples/pdf-converter-article-title-with-author-and-date.rb
+++ docs/modules/extend/examples/pdf-converter-article-title-with-author-and-date.rb
@@ -3,14 +3,26 @@ class PDFConverterArticleTitleWithAuthorAndDate < (Asciidoctor::Converter.for 'p
 
   def ink_general_heading doc, title, opts
     return super unless opts[:role] == :doctitle # <1>
-    ink_document_title title # <2>
+    ink_document_title title, opts # <2>
     ink_document_metainfo doc
     ink_titlesection_body_separator
   end
 
-  def ink_document_title title
-    theme_font :heading_doctitle do
-      ink_prose title, align: :center
+  def ink_document_title title, opts
+    if (top_margin = @theme.heading_h1_margin_page_top || @theme.heading_margin_page_top) > 0
+      move_down top_margin
+    end
+    pad_box @theme.heading_h1_padding do
+      if (transform = resolve_text_transform opts)
+        title = transform_text title, transform
+      end
+      if (inherited = apply_text_decoration font_styles, :heading, 1).empty?
+        inline_format_opts = true
+      else
+        inline_format_opts = [{ inherited: inherited }]
+      end
+      typeset_text_opts = { color: @font_color, inline_format: inline_format_opts }.merge opts
+      typeset_text title, (calc_line_metrics (opts.delete :line_height) || @base_line_height), typeset_text_opts 
     end
   end
 
@@ -26,6 +38,6 @@ class PDFConverterArticleTitleWithAuthorAndDate < (Asciidoctor::Converter.for 'p
   end
 
   def ink_titlesection_body_separator
-    theme_margin :heading_doctitle, :bottom
+    margin_bottom @theme[:heading_h1_margin_bottom] || @theme.heading_margin_bottom
   end
 end
