5f44ad256d1e7e7ca0ebcb6e1cbf1ee59b5949f8
Dan Allen <dan.j.allen@gmail.com>
2023-10-08 23:07:46 -0600

    resolves #2455 add extended converter use case to place colophon before TOC (PR #2456)

---
+++ docs/modules/extend/examples/pdf-converter-colophon-placement.rb
@@ -0,0 +1,20 @@
+class AsciidoctorPDFExtensions < (Asciidoctor::Converter.for 'pdf')
+  register_for 'pdf'
+
+  def ink_toc doc, num_levels, toc_page_number, start_cursor, num_front_matter_pages = 0
+    colophon = (doc.instance_variable_get :@colophon) || (doc.sections.find {|sect| sect.sectname == 'colophon' })
+    return super unless colophon
+    go_to_page toc_page_number unless (page_number == toc_page_number) || scratch?
+    if scratch?
+      (doc.instance_variable_set :@colophon, colophon).parent.blocks.delete colophon
+    else
+      # if doctype=book and media=prepress, use blank page before table of contents
+      go_to_page page_number.pred if @ppbook
+      convert_section colophon
+      go_to_page page_number.next
+    end
+    offset = @ppbook ? 0 : 1
+    toc_page_numbers = super doc, num_levels, (toc_page_number + offset), start_cursor, num_front_matter_pages
+    scratch? ? ((toc_page_numbers.begin - offset)..toc_page_numbers.end) : toc_page_numbers
+  end
+end
