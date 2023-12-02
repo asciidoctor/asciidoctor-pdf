bd1444f902498dfcfcf446cdf59a158b7e65e9a6
Dan Allen <dan.j.allen@gmail.com>
2023-12-02 01:56:58 -0700

    fix lint error

--- lib/asciidoctor/pdf/converter.rb
+++ lib/asciidoctor/pdf/converter.rb
@@ -4164,7 +4164,7 @@ module Asciidoctor
             str_to_pt width
           end
         elsif attrs.key? 'scale'
-          %(#{attrs['scale']}).extend ImageWidth
+          attrs['scale'].to_s.extend ImageWidth
         elsif attrs.key? 'scaledwidth'
           # NOTE: the parser automatically appends % if value is unitless
           if (width = attrs['scaledwidth']).end_with? '%'
