73b12503108b9219522536593528c59d8065c85a
Dan Allen <dan.j.allen@gmail.com>
2024-02-05 03:18:41 -0700

    remove outdated comment on extended converter for removing running content on empty pages

--- docs/modules/extend/examples/pdf-converter-no-running-content-empty-page.rb
+++ docs/modules/extend/examples/pdf-converter-no-running-content-empty-page.rb
@@ -5,13 +5,13 @@ class NoRunningContentOnEmptyPageConverter < (Asciidoctor::Converter.for 'pdf')
     state.pages.each do |page_|
       page_.imported if page_.empty?
     end
-    # or
-    #(1..page_count).each do |pgnum|
-    #  go_to_page pgnum
+    # or you can switch to each page first in case you need to use additional logic
+    # pgnum = page_number
+    #(1..page_count).each do |pgnum_|
+    #  go_to_page pgnum_
     #  page.imported if page.empty?
-    #  ## to write content to an empty page, use this next statement instead
-    #  #ink_prose ' ' if page.empty?
     #end
+    # go_to_page pgnum
     super
   end
 end
