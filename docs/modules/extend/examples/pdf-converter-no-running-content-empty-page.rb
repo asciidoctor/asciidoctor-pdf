02d3b1f04b35233dfb52bac9fcc023fd624c96d0
Dan Allen <dan.j.allen@gmail.com>
2024-02-05 03:32:48 -0700

    document another way to turn off running content on empty pages

--- docs/modules/extend/examples/pdf-converter-no-running-content-empty-page.rb
+++ docs/modules/extend/examples/pdf-converter-no-running-content-empty-page.rb
@@ -5,13 +5,23 @@ class NoRunningContentOnEmptyPageConverter < (Asciidoctor::Converter.for 'pdf')
     state.pages.each do |page_|
       page_.imported if page_.empty?
     end
+
     # or you can switch to each page first in case you need to use additional logic
+
     # pgnum = page_number
     #(1..page_count).each do |pgnum_|
     #  go_to_page pgnum_
     #  page.imported if page.empty?
     #end
     # go_to_page pgnum
+
+    # or you can add the page numbers to the sets of page numbers on which the running content is disabled
+    #state.pages.each_with_index do |page_, idx|
+    #  next unless page_.empty?
+    #  pgnum = idx + 1
+    #  @disable_running_content[:header] << pgnum
+    #  @disable_running_content[:footer] << pgnum
+    #end
     super
   end
 end
