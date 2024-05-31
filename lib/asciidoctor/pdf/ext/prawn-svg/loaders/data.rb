757b386f6f9dcde539d23467a1deacccbe525996
Dan Allen <dan.j.allen@gmail.com>
2024-05-31 02:14:56 -0600

    resolves #2512 support base64-encoded SVG image reference in SVG

--- lib/asciidoctor/pdf/ext/prawn-svg/loaders/data.rb
+++ lib/asciidoctor/pdf/ext/prawn-svg/loaders/data.rb
@@ -2,5 +2,5 @@
 
 class Prawn::SVG::Loaders::Data
   remove_const :REGEXP
-  REGEXP = %r(\Adata:image/(?:png|jpe?g);base64(?:;[a-z0-9]+)*,)i
+  REGEXP = %r(\Adata:image/(?:png|jpe?g|svg\+xml);base64(?:;[a-z0-9]+)*,)i
 end
