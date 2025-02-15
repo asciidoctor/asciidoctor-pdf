163596b2e92b8e0f017a94ce964132ee47546ae7
Dan Allen <dan.j.allen@gmail.com>
2025-02-15 03:05:03 -0700

    update hexapdf integration to work with latest release

--- docs/modules/ROOT/examples/optimizer-hexapdf.rb
+++ docs/modules/ROOT/examples/optimizer-hexapdf.rb
@@ -9,22 +9,20 @@ class OptimizerHexaPDF < Asciidoctor::PDF::Optimizer::Base
     app.instance_variable_set :@force, true
     @optimize = app.main_command.commands['optimize']
     @optimize.singleton_class.attr_reader :out_options
-    options = @optimize.out_options
-    options.compress_pages = true
-    #options.object_streams = :preserve
-    #options.xref_streams = :preserve
-    #options.streams = :preserve # or :uncompress
+    @optimize.out_options[:compress_pages] = true
+    #@optimize.out_options[:object_streams] = :preserve
+    #@optimize.out_options[:xref_streams] = :preserve
+    #@optimize.out_options[:streams] = :preserve # or :uncompress
   end
 
   def optimize_file path
     @optimize.execute path, path
+    @optimize.out_options[:compress_pages] = false
     nil
   rescue
     # retry without page compression, which can sometimes fail
-    @optimize.out_options.compress_pages = false
+    @optimize.out_options[:compress_pages] = false
     @optimize.execute path, path
     nil
-  ensure
-    @optimize.out_options.compress_pages = false
   end
 end
