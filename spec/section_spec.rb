3fd09be3183b646717eb70cb7227cb58e4e786e8
Dan Allen <dan.j.allen@gmail.com>
2025-10-22 17:45:36 -0600

    update orphan test to assert page number of section

--- spec/section_spec.rb
+++ spec/section_spec.rb
@@ -1293,9 +1293,17 @@ describe 'Asciidoctor::PDF::Converter - Section' do
     end
   end
 
-  it 'should keep section that spans pages with first block of unbreakable content if breakable option is set on section' do
+  it 'should keep section that advanced page with first block of unbreakable content if breakable option is set on section' do
     with_content_spacer 200, 675 do |spacer_path|
-      pdf = to_pdf <<~END, pdf_theme: { heading_min_height_after: nil }, analyze: true
+      pdf_theme = {
+        heading_min_height_after: nil,
+        toc_font_color: 'a0a0a0',
+      }
+      pdf = to_pdf <<~END, pdf_theme: pdf_theme, analyze: true
+      :toc:
+
+      <<<
+
       == Section A
 
       image::#{spacer_path}[pdfwidth=70mm]
@@ -1313,10 +1321,14 @@ describe 'Asciidoctor::PDF::Converter - Section' do
       --
       END
 
-      section_b_text = pdf.find_unique_text 'Section B'
-      (expect section_b_text[:page_number]).to be 2
-      content_text = pdf.find_unique_text 'keep'
-      (expect content_text[:page_number]).to be 2
+      toc_entries = (pdf.lines (pdf.find_text page_number: 1))[1..-1]
+      (expect toc_entries).to have_size 2
+      (expect toc_entries[0]).to end_with '2'
+      (expect toc_entries[1]).to end_with '3'
+      section_b_text = pdf.find_unique_text 'Section B', font_color: '333333'
+      (expect section_b_text[:page_number]).to be 3
+      content_text = pdf.find_unique_text 'keep', font_color: '333333'
+      (expect content_text[:page_number]).to be 3
     end
   end
 
